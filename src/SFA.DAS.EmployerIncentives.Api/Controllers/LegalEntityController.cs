0dfa71bc22bd1639eed7ed8810020661d93060f3
Ruslan <ruslanss@gmail.com>
2020-06-26 10:22:01 +0100

    [EI-73] Add inner API project

---
+++ src/SFA.DAS.EmployerIncentives.Api/Controllers/LegalEntityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.EmployerIncentives.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LegalEntityController : ControllerBase
+    {
+
+        [HttpPost]
+        public Task<IActionResult> Add()
+        {
+           throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public Task<IActionResult> Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
