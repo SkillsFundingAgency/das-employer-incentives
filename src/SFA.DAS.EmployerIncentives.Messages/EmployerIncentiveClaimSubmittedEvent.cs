90d3a725e7de3ea9c68c7f080dd5b0ddadb08152
Dave Read <david.read@digital.education.gov.uk>
2020-06-29 17:27:23 +0100

    EI-72 add library for NServiceBus trigger messages

---
+++ src/SFA.DAS.EmployerIncentives.Messages/EmployerIncentiveClaimSubmittedEvent.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.NServiceBus;
+using System;
+
+namespace SFA.DAS.EmployerIncentives.Messages
+{
+    public class EmployerIncentiveClaimSubmittedEvent : Event
+    {
+        public Guid IncentiveClaimApprenticeshipId { get; set; }
+    }
+}
