b7020ea599781d02c459c356815c4e02967d6411
Ruslan <ruslanss@gmail.com>
2020-06-29 18:43:25 +0100

    EI-73 Add placeholder Command Dispatcher and Query Provider

--- src/SFA.DAS.EmployerIncentives.Queries/IQueryHandler.cs
+++ src/SFA.DAS.EmployerIncentives.Queries/IQueryHandler.cs
@@ -1,6 +1,9 @@
-﻿namespace SFA.DAS.EmployerIncentives.Queries
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Queries
 {
-    public interface IQueryHandler<T> where T: IQuery
+    public interface IQueryHandler<in TQuery>
     {
+        Task<TResponse> Execute<TResponse>(TQuery query);
     }
 }
--- src/SFA.DAS.EmployerIncentives.Queries/IQueryProvider.cs
+++ src/SFA.DAS.EmployerIncentives.Queries/IQueryProvider.cs
@@ -1,7 +1,9 @@
-﻿namespace SFA.DAS.EmployerIncentives.Queries
-{
-    public class IQueryProvider
-    {
+﻿using System.Threading.Tasks;
 
+namespace SFA.DAS.EmployerIncentives.Queries
+{
+    public interface IQueryProvider
+    {
+        Task<TResponse> Execute<TResponse, TQuery>(TQuery query);
     }
 }
