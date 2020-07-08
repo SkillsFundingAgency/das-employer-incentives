2191627f7f8979d736989b63cdf788701b2b4493
Ruslan <ruslanss@gmail.com>
2020-07-08 19:31:19 +0100

    EI-98 Return 404 Not Found for non-existing data

--- src/tests/SFA.DAS.EmployerIncentives.Api.UnitTests/Account/WhenGetLegalEntities.cs
+++ src/tests/SFA.DAS.EmployerIncentives.Api.UnitTests/Account/WhenGetLegalEntities.cs
@@ -1,32 +1,38 @@
+using AutoFixture;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Abstractions;
 using SFA.DAS.EmployerIncentives.Abstractions.Queries;
 using SFA.DAS.EmployerIncentives.Api.Controllers;
 using SFA.DAS.EmployerIncentives.Queries.Account;
+using System;
+using System.Net;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace SFA.DAS.EmployerIncentives.Api.UnitTests.Account
 {
-    public class AccountQueryControllerTests
+    public class WhenGetLegalEntities
     {
         private AccountQueryController _sut;
         private Mock<IQueryDispatcher> _queryDispatcherMock;
+        private Fixture _fixture;
 
         [SetUp]
         public void Setup()
         {
             _queryDispatcherMock = new Mock<IQueryDispatcher>();
-
+            _fixture = new Fixture();
             _sut = new AccountQueryController(_queryDispatcherMock.Object);
         }
 
         [Test]
-        public async Task When_GetLegalEntities_requested_Then_data_is_returned()
+        public async Task Then_data_is_returned_for_existing_account()
         {
             // Arrange
             const long accountId = 7;
-            var expected = new GetLegalEntitiesResponse();
+            var expected = new GetLegalEntitiesResponse { LegalEntities = _fixture.CreateMany<LegalEntityDto>() };
 
             _queryDispatcherMock.Setup(x => x.Send<GetLegalEntitiesRequest, GetLegalEntitiesResponse>(
                     It.Is<GetLegalEntitiesRequest>(r => r.AccountId == accountId)))
@@ -37,6 +43,24 @@ namespace SFA.DAS.EmployerIncentives.Api.UnitTests.Account
 
             // Assert
             actual.Should().Be(expected);
+        }
+
+        [Test]
+        public void Then_error_returned_for_non_existing_account()
+        {
+            // Arrange
+            const long accountId = 7;
+            var expected = new GetLegalEntitiesResponse();
+
+            _queryDispatcherMock.Setup(x => x.Send<GetLegalEntitiesRequest, GetLegalEntitiesResponse>(
+                    It.Is<GetLegalEntitiesRequest>(r => r.AccountId == accountId)))
+                .ReturnsAsync(expected);
+
+            // Act
+             Action act = () =>  _sut.GetLegalEntities(accountId);
+
+            // Assert
+            act.Should().Throw<HttpResponseException>();//.Where(x => x.Response.StatusCode == HttpStatusCode.NotFound);
         }
 
     }
