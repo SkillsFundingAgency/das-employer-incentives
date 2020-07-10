79b0e49957576d040326812284262bbc232fa265
Kevin <kevin@stephek.co.uk>
2020-07-10 12:32:26 +0100

    EI-20-correct-exception-text

--- src/SFA.DAS.EmployerIncentives.Commands/Services/MultiEventPublisherWithLimit.cs
+++ src/SFA.DAS.EmployerIncentives.Commands/Services/MultiEventPublisherWithLimit.cs
@@ -39,7 +39,7 @@ namespace SFA.DAS.EmployerIncentives.Commands.Services
                 var result = await task;
                 if (result.FinalException != null)
                 {
-                    errors.Add(new Exception($"Error publishing {typeof(T).Name} for Message {Newtonsoft.Json.JsonConvert.SerializeObject(result.Context["Message"]) }", result.FinalException));
+                    errors.Add(new Exception($"Error publishing message {typeof(T).Name} with body {Newtonsoft.Json.JsonConvert.SerializeObject(result.Context["Message"]) }", result.FinalException));
                 }
             }
             if (errors.Count > 0)
