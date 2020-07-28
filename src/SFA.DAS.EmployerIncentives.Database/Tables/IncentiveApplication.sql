ec5386154fe0228474b7cd17d4244189f00a8a54
Michael Young <mick.young1981@googlemail.com>
2020-07-28 08:45:59 +0100

    Added database tables.

---
+++ src/SFA.DAS.EmployerIncentives.Database/Tables/IncentiveApplication.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[IncentiveApplication]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[AccountId] INT NOT NULL,
+	[AccountLegalEntityId] INT NOT NULL,
+	[DateCreated] DATETIME NOT NULL,
+	[Status] NVARCHAR(50) NOT NULL,
+	[DateSubmitted] DATETIME NULL,
+	[SubmittedBy] NVARCHAR(50) NULL
+)
---
+++ src/SFA.DAS.EmployerIncentives.Database/Tables/IncentiveApplicationApprenticeship.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[IncentiveApplicationApprenticeship]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[IncentiveApplicationId] UNIQUEIDENTIFIER NOT NULL,
+	[ApprenticeshipId] INT NOT NULL,
+	[FirstName] NVARCHAR(100) NOT NULL,
+	[LastName] NVARCHAR(100) NOT NULL,
+	[DateOfBirth] DATETIME NOT NULL,
+	[ULN] INT NOT NULL,
+	[PlannedStartDate] DATETIME NOT NULL,
+	[ApprenticeshipEmployerTypeOnApproval] NVARCHAR(50)
+)
