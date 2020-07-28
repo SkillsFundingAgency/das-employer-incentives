0d4ec3359b44a1d08c2bb1a4fb67171928fa9752
Michael Young <mick.young1981@googlemail.com>
2020-07-28 09:46:34 +0100

    Added aggregate root

---
+++ src/SFA.DAS.EmployerIncentives.Domain.UnitTests/IncentiveApplicationTests/WhenCreated.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Enums;
+
+namespace SFA.DAS.EmployerIncentives.Domain.UnitTests.IncentiveApplicationTests
+{
+    public class WhenCreated
+    {
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void Then_the_id_is_set()
+        {
+            // Arrange
+            var id = _fixture.Create<Guid>();
+
+            // Act
+            var application = IncentiveApplication.IncentiveApplication.New(id, _fixture.Create<long>(), _fixture.Create<long>());
+
+            // Assert
+            application.Id.Should().Be(id);
+        }
+
+        [Test]
+        public void Then_the_account_id_is_set()
+        {
+            // Arrange
+            var accountId = _fixture.Create<long>();
+
+            // Act
+            var application = IncentiveApplication.IncentiveApplication.New(_fixture.Create<Guid>(), accountId, _fixture.Create<long>());
+
+            // Assert
+            application.AccountId.Should().Be(accountId);
+        }
+
+        [Test]
+        public void Then_the_account_legal_entity_id_is_set()
+        {
+            // Arrange
+            var accountLegalEntityId = _fixture.Create<long>();
+
+            // Act
+            var application = IncentiveApplication.IncentiveApplication.New(_fixture.Create<Guid>(), _fixture.Create<long>(), accountLegalEntityId);
+
+            // Assert
+            application.AccountLegalEntityId.Should().Be(accountLegalEntityId);
+        }
+
+        [Test]
+        public void Then_the_status_is_set_to_in_progress()
+        {
+            // Act
+            var application = IncentiveApplication.IncentiveApplication.New(_fixture.Create<Guid>(), _fixture.Create<long>(), _fixture.Create<long>());
+
+            // Assert
+            application.Status.Should().Be(IncentiveApplicationStatus.InProgress);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Domain/IncentiveApplication/IncentiveApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SFA.DAS.EmployerIncentives.Abstractions.Domain;
+using SFA.DAS.EmployerIncentives.Domain.IncentiveApplication.Models;
+using SFA.DAS.EmployerIncentives.Enums;
+
+namespace SFA.DAS.EmployerIncentives.Domain.IncentiveApplication
+{
+    public sealed class IncentiveApplication : AggregateRoot<Guid, IncentiveApplicationModel>
+    {
+        public long AccountId => Model.AccountId;
+        public long AccountLegalEntityId => Model.AccountLegalEntityId;
+        public DateTime DateCreated => Model.DateCreated;
+        public IncentiveApplicationStatus Status => Model.Status;
+
+        public static IncentiveApplication New(Guid id, long accountId, long accountLegalEntityId)
+        {
+            return new IncentiveApplication(id, new IncentiveApplicationModel { Id = id, AccountId = accountId, AccountLegalEntityId = accountLegalEntityId, DateCreated = DateTime.Now, Status = IncentiveApplicationStatus.InProgress }, true);
+        }
+
+        private IncentiveApplication(Guid id, IncentiveApplicationModel model, bool isNew = false) : base(id, model, isNew)
+        {
+        }
+    }
+}
