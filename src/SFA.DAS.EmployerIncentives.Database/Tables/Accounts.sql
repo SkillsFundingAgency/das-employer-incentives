4aca12413f8fad3514a381fd1e522c8373eec971
Michael Young <mick.young1981@googlemail.com>
2020-07-29 14:59:12 +0100

    Add new column.

--- src/SFA.DAS.EmployerIncentives.Database/Tables/Accounts.sql
+++ src/SFA.DAS.EmployerIncentives.Database/Tables/Accounts.sql
@@ -4,6 +4,7 @@
 	[AccountLegalEntityId] BIGINT  NOT NULL,
 	[LegalEntityId] BIGINT  NOT NULL,
 	[LegalEntityName] VARCHAR(MAX)  NOT NULL,
+	[HasSignedIncentiveTerms] BIT NOT NULL DEFAULT(0),
 	CONSTRAINT PK_Accounts PRIMARY KEY NONCLUSTERED ([Id], [AccountLegalEntityId])
 )
 GO;
