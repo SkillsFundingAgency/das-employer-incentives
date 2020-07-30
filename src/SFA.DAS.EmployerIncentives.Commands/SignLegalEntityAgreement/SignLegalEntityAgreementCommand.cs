9adba7617b7e3eb864dbce6ee5f6dd9002c5bee9
Michael Young <mick.young1981@googlemail.com>
2020-07-30 09:43:24 +0100

    Added command validator

---
+++ src/SFA.DAS.EmployerIncentives.Commands/SignLegalEntityAgreement/SignLegalEntityAgreementCommand.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+
+namespace SFA.DAS.EmployerIncentives.Commands.SignLegalEntityAgreement
+{    
+    public class SignLegalEntityAgreementCommand : ICommand
+    {
+        public long AccountLegalEntityId { get; }
+        public int AgreementVersion { get; }
+
+        public SignLegalEntityAgreementCommand(
+            long accountLegalEntityId,
+            int agreementVersion)
+        {
+            AccountLegalEntityId = accountLegalEntityId;
+            AgreementVersion = agreementVersion;
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Commands/SignLegalEntityAgreement/SignLegalEntityAgreementCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+
+namespace SFA.DAS.EmployerIncentives.Commands.SignLegalEntityAgreement
+{
+    public class SignLegalEntityAgreementCommandValidator : IValidator<SignLegalEntityAgreementCommand>
+    {
+        public Task<ValidationResult> Validate(SignLegalEntityAgreementCommand item)
+        {
+            var result = new ValidationResult();
+
+            if (item.AccountLegalEntityId == default)
+            {
+                result.AddError("AccountLegalEntityId", "Is not set");
+            }
+
+            if (item.AgreementVersion == default)
+            {
+                result.AddError("AgreementVersion", "Is not set");
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
