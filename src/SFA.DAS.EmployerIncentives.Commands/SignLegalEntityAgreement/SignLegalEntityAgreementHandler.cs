a4c3d57839c9fb7ad312e6401bac0d76dbd9dc78
Michael Young <mick.young1981@googlemail.com>
2020-07-30 11:25:26 +0100

    Fixed incorrect test

--- src/SFA.DAS.EmployerIncentives.Commands/SignLegalEntityAgreement/SignLegalEntityAgreementHandler.cs
+++ src/SFA.DAS.EmployerIncentives.Commands/SignLegalEntityAgreement/SignLegalEntityAgreementHandler.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.EmployerIncentives.Abstractions.Commands;
 using SFA.DAS.EmployerIncentives.Commands.Persistence;
-using SFA.DAS.EmployerIncentives.Domain.Accounts;
 
 namespace SFA.DAS.EmployerIncentives.Commands.SignLegalEntityAgreement
 {
@@ -19,22 +18,6 @@ namespace SFA.DAS.EmployerIncentives.Commands.SignLegalEntityAgreement
         public async Task Handle(SignLegalEntityAgreementCommand command, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
-            var account = await _domainRepository.Find(command.AccountId);
-            if (account != null) 
-            {
-                if (account.GetLegalEntity(command.AccountLegalEntityId) != null)
-                {
-                    return; // already created
-                }
-            }
-            else
-            {
-                account = Account.New(command.AccountId);
-            }
-
-            account.AddLegalEntity(command.AccountLegalEntityId, LegalEntity.New(command.LegalEntityId, command.Name));
-
-            await _domainRepository.Save(account);
         }
     }
 }
---
+++ src/tests/SFA.DAS.EmployerIncentives.Commands.UnitTests/SignLegalEntityAgreement/Handlers/WhenSigningAnAgreement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Commands.Persistence;
+using SFA.DAS.EmployerIncentives.Commands.SignLegalEntityAgreement;
+using SFA.DAS.EmployerIncentives.Domain.Accounts;
+using SFA.DAS.EmployerIncentives.Domain.Accounts.Models;
+
+namespace SFA.DAS.EmployerIncentives.Commands.UnitTests.SignLegalEntityAgreement.Handlers
+{
+    public class WhenSigningAnAgreement
+    {
+        private SignLegalEntityAgreementCommandHandler _sut;
+        private Mock<IAccountDomainRepository> _mockDomainRespository;
+        
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+
+            _mockDomainRespository = new Mock<IAccountDomainRepository>();
+            
+            _sut = new SignLegalEntityAgreementCommandHandler(_mockDomainRespository.Object);
+        }
+
+        [Test]
+        public async Task Then_the_agreement_status_of_the_legal_entity_is_updated()
+        {
+            //Arrange
+            var command = _fixture.Create<SignLegalEntityAgreementCommand>();
+            var expectedAccount = Account.Create(new AccountModel { Id = 1, LegalEntityModels = new Collection<LegalEntityModel>() });
+            _mockDomainRespository
+                .Setup(m => m.Find(command.AccountId))
+                .ReturnsAsync(expectedAccount);
+
+            //Act
+            await _sut.Handle(command);
+
+            //Assert
+            _mockDomainRespository.Verify(m => m.Save(expectedAccount), Times.Once);
+        }
+    }
+}
