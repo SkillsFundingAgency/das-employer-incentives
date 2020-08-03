8f7f40f1f74cebf2995dc79e1e471f55c0fa45aa
Michael Young <mick.young1981@googlemail.com>
2020-08-03 14:11:35 +0100

    Added query handler.

---
+++ src/SFA.DAS.EmployerIncentives.Queries/NewApprenticeIncentive/GetApplication/GetApplicationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFA.DAS.EmployerIncentives.Abstractions.Queries;
+
+namespace SFA.DAS.EmployerIncentives.Queries.NewApprenticeIncentive.GetApplication
+{
+    public class GetApplicationRequest : IQuery
+    {
+        public Guid ApplicationId { get; }
+
+        public GetApplicationRequest(Guid applicationId)
+        {
+            ApplicationId = applicationId;
+        }
+    }
+}
