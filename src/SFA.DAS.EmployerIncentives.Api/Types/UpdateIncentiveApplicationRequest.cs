f6ea43e24123fac11412356e80060bc7a108bc6b
Ruslan <ruslanss@gmail.com>
2020-08-10 09:59:40 +0100

    EI-203 Remove AccountLegalEntity from Update Incentive Application

--- src/SFA.DAS.EmployerIncentives.Api/Types/UpdateIncentiveApplicationRequest.cs
+++ src/SFA.DAS.EmployerIncentives.Api/Types/UpdateIncentiveApplicationRequest.cs
@@ -8,7 +8,6 @@ namespace SFA.DAS.EmployerIncentives.Api.Types
     {
         public Guid IncentiveApplicationId { get; set; }
         public long AccountId { get; set; }
-        public long AccountLegalEntityId { get; set; }
         public IEnumerable<IncentiveApplicationApprenticeshipDto> Apprenticeships { get; set; }
     }
 }
