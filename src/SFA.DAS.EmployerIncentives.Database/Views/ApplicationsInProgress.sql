1874093140fd416fca6f43eb621c573c373c56a9
Dave Read <david.read@digital.education.gov.uk>
2020-08-24 12:15:04 +0100

    EI-21 Grafana dashboard views for applications

---
+++ src/SFA.DAS.EmployerIncentives.Database/Views/ApplicationsInProgress.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[ApplicationsInProgress]
+	AS 
+SELECT
+  DATEDIFF(second, '1970-01-01', DateCreated) AS time,
+  count(*) as value,
+  [Status] as metric
+FROM
+  IncentiveApplication
+WHERE
+[Status] = 'InProgress'
+GROUP BY DateCreated, [Status]
+
