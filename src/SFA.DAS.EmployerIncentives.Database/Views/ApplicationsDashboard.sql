7e927ecd82d6112457cd67bd4cd58e8e5fb28167
Dave Read <david.read@digital.education.gov.uk>
2020-08-24 14:50:08 +0100

    EI-21 combine dashboard data into single view

---
+++ src/SFA.DAS.EmployerIncentives.Database/Views/ApplicationsDashboard.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[ApplicationsDashboard]
+	AS 
+	SELECT 
+	ia.Id, 
+	ia.DateCreated, 
+	ia.[Status], 
+	ia.DateSubmitted, 
+	iaa.PlannedStartDate, 
+	iaa.TotalIncentiveAmount,
+	iaa.ApprenticeshipEmployerTypeOnApproval
+	FROM IncentiveApplication ia
+	INNER JOIN IncentiveApplicationApprenticeship iaa
+	oN ia.Id = iaa.IncentiveApplicationId
