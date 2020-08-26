0b0d9cca573d5de8abbdad9af4a89fc9efc1eacf
Michael Young <mick.young1981@googlemail.com>
2020-08-26 10:45:54 +0100

    Add new query handler

---
+++ src/SFA.DAS.EmployerIncentives.Queries/NewApprenticeIncentive/GetIncentiveDetails/GetIncentiveDetailsResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerIncentives.Queries.NewApprenticeIncentive.GetIncentiveDetails
+{
+    public class GetIncentiveDetailsResponse
+    {
+        public DateTime EligibilityStartDate { get; }
+        public DateTime EligibilityEndDate { get; }
+
+        public GetIncentiveDetailsResponse(DateTime eligibilityStartDate, DateTime eligibilityEndDate)
+        {
+            EligibilityStartDate = eligibilityStartDate;
+            EligibilityEndDate = eligibilityEndDate;
+        }
+    }
+}
---
+++ src/tests/SFA.DAS.EmployerIncentives.Queries.UnitTests/NewApprenticeIncentive/Handlers/WhenHandlingGetIncentiveDetailsQuery.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Queries.NewApprenticeIncentive.GetIncentiveDetails;
+
+namespace SFA.DAS.EmployerIncentives.Queries.UnitTests.NewApprenticeIncentive.Handlers
+{
+    public class WhenHandlingGetIncentiveDetailsQuery
+    {
+        private GetIncentiveDetailsQueryHandler _sut;
+        
+        [SetUp]
+        public void Arrange()
+        {
+            _sut = new GetIncentiveDetailsQueryHandler();
+        }
+
+        [Test]
+        public async Task Then_the_incentive_eligibility_is_returned()
+        {
+            //Arrange
+            var query = new GetIncentiveDetailsRequest();
+            
+            //Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            //Assert
+            result.EligibilityStartDate.Should().Be(ValueObjects.NewApprenticeIncentive.EligibilityStartDate);
+            result.EligibilityEndDate.Should().Be(ValueObjects.NewApprenticeIncentive.EligibilityEndDate);
+        }
+    }
+}
