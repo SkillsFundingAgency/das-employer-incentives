23e45c2ce388c6efb7166e2b378a07d3d13a3c1a
Michael Young <mick.young1981@googlemail.com>
2020-09-02 14:42:37 +0100

    Added empty validator

---
+++ src/SFA.DAS.EmployerIncentives.Commands/UpdateVrfCaseDetailsForExistingSubmission/UpdateVrfCaseDetailsForExistingSubmissionCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+
+namespace SFA.DAS.EmployerIncentives.Commands.UpdateVrfCaseDetailsForExistingSubmission
+{
+    public class UpdateVrfCaseDetailsForExistingSubmissionCommandValidator : IValidator<UpdateVrfCaseDetailsForExistingSubmissionCommand>
+    {
+        public Task<ValidationResult> Validate(UpdateVrfCaseDetailsForExistingSubmissionCommand item)
+        {
+            return Task.FromResult(new ValidationResult());
+        }
+    }
+}
