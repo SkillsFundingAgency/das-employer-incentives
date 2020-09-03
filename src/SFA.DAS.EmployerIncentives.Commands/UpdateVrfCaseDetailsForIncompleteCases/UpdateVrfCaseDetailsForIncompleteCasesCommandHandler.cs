7ceacb3bd678fff70155ec647740efa109cfd5f2
Michael Young <mick.young1981@googlemail.com>
2020-09-03 15:08:11 +0100

    Added repository

---
+++ src/SFA.DAS.EmployerIncentives.Commands/UpdateVrfCaseDetailsForIncompleteCases/UpdateVrfCaseDetailsForIncompleteCasesCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+
+namespace SFA.DAS.EmployerIncentives.Commands.UpdateVrfCaseDetailsForIncompleteCases
+{
+    public class UpdateVrfCaseDetailsForIncompleteCasesCommandHandler : ICommandHandler<UpdateVrfCaseDetailsForIncompleteCasesCommand>
+    {
+        public UpdateVrfCaseDetailsForIncompleteCasesCommandHandler()
+        {
+        }
+
+        public async Task Handle(UpdateVrfCaseDetailsForIncompleteCasesCommand command, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ src/SFA.DAS.EmployerIncentives.Commands/UpdateVrfCaseDetailsForIncompleteCases/UpdateVrfCaseDetailsForIncompleteCasesCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+
+namespace SFA.DAS.EmployerIncentives.Commands.UpdateVrfCaseDetailsForIncompleteCases
+{
+    public class UpdateVrfCaseDetailsForIncompleteCasesCommandValidator : IValidator<UpdateVrfCaseDetailsForIncompleteCasesCommand>
+    {
+        public Task<ValidationResult> Validate(UpdateVrfCaseDetailsForIncompleteCasesCommand item)
+        {
+            return Task.FromResult(new ValidationResult());
+        }
+    }
+}
