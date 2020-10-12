6769eb858ab641deb4fc40fb02a1e49493fa70b2
Kevin <kevin@stephek.co.uk>
2020-10-12 13:55:38 +0100

    Ei-427-more-tests

---
+++ src/SFA.DAS.EmployerIncentives.Domain.UnitTests/CollectionCalendar/WhenGetPeriod.cs
@@ -0,0 +1,62 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.EmployerIncentives.Domain.UnitTests.CollectionCalendarTests
+{
+    public class GetPeriod
+    {
+        private CollectionCalendar _sut;
+        private List<CollectionPeriod> _collectionPeriods;
+        private Fixture _fixture;
+        private DateTime testDate;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+
+            testDate = DateTime.Now;
+
+            var period1 = new CollectionPeriod(1, (byte)testDate.AddMonths(13).Month, (short)testDate.AddMonths(13).Year, testDate.AddMonths(13));
+            var period2 = new CollectionPeriod(2, (byte)testDate.Month, (short)testDate.Year, testDate);
+            var period3 = new CollectionPeriod(3, (byte)testDate.AddMonths(-13).Month, (short)testDate.AddMonths(-13).Year, testDate.AddMonths(-13));
+
+            _collectionPeriods = new List<CollectionPeriod>() { period1, period2, period3 };
+
+            _sut = new CollectionCalendar(_collectionPeriods);
+        }
+
+        [Test]
+        public void Then_the_latest_collection_period_is_returned_for_a_given_date()
+        {
+            // Arrange
+            var date = testDate.AddDays(1);
+
+            // Act
+            var period = _sut.GetPeriod(date);
+
+            // Assert
+            period.PeriodNumber.Should().Be(2);
+            period.CalendarMonth.Should().Be((byte)testDate.Month);
+            period.CalendarYear.Should().Be((short)testDate.Year);
+            period.OpenDate.Should().Be(testDate);
+        }
+
+        [Test]
+        public void Then_null_is_returned_if_the_given_date_is_outside_the_collection_date_range()
+        {
+            // Arrange
+            var date = testDate.AddMonths(-13).AddDays(-1);
+
+            // Act
+            var period = _sut.GetPeriod(date);
+
+            // Assert
+            period.Should().BeNull();
+        }
+    }
+}
