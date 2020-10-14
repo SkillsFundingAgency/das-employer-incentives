5a5729cc6c467a8183660b13b4ce96a15c5a4ffe
Michael Young <mick.young1981@googlemail.com>
2020-10-14 16:02:14 +0100

    Added sub orchestrator

---
+++ src/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess/CalculatePaymentsForAccountLegalEntityOrchestrator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.EmployerIncentives.Functions.PaymentsProcess
+{
+    public class CalculatePaymentsForAccountLegalEntityOrchestrator
+    {
+        private ILogger<CalculatePaymentsForAccountLegalEntityOrchestrator> _logger;
+
+        public CalculatePaymentsForAccountLegalEntityOrchestrator(ILogger<CalculatePaymentsForAccountLegalEntityOrchestrator> logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName("CalculatePaymentsForAccountLegalEntityOrchestrator")]
+        public async Task RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var accountLegalEntityCollectionPeriod = context.GetInput<AccountLegalEntityCollectionPeriod>();
+            var accountLegalEntityId = accountLegalEntityCollectionPeriod.AccountLegalEntityId;
+            var collectionPeriod = accountLegalEntityCollectionPeriod.CollectionPeriod;
+
+            _logger.LogInformation($"Calculate Payments process started for account legal entity {accountLegalEntityId}, collection period {collectionPeriod}", new { accountLegalEntityId, collectionPeriod  });
+
+            var pendingPayments = await context.CallActivityAsync<List<Guid>>("GetPendingPaymentsForAccountLegalEntity", accountLegalEntityCollectionPeriod);
+
+            foreach (var pendingPaymentId in pendingPayments)
+            {
+                _logger.LogInformation($"Request made to validate pending payment for pending payment id {pendingPaymentId}", new { accountLegalEntityId, collectionPeriod, pendingPayment = pendingPaymentId });
+            }
+
+            _logger.LogInformation($"Calculate Payments process completed for account legal entity {accountLegalEntityId}, collection period {collectionPeriod}", new { accountLegalEntityId, collectionPeriod });
+        }
+    }
+}
