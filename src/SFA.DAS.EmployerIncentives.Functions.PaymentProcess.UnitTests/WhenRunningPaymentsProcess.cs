f95c7a2733782970693457113d4942716fd5bbd1
Michael Young <mick.young1981@googlemail.com>
2020-10-14 16:30:13 +0100

    Added call to sub orchestrator

--- src/SFA.DAS.EmployerIncentives.Functions.PaymentProcess.UnitTests/WhenRunningPaymentsProcess.cs
+++ src/SFA.DAS.EmployerIncentives.Functions.PaymentProcess.UnitTests/WhenRunningPaymentsProcess.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
@@ -15,6 +17,7 @@ namespace SFA.DAS.EmployerIncentives.Functions.PaymentProcess.UnitTests
         private CollectionPeriod _collectionPeriod;
         private Mock<IDurableOrchestrationContext> _mockOrchestrationContext;
         private IncentivePaymentOrchestrator _orchestrator;
+        private List<long> _legalEntities;
 
         [SetUp]
         public void Setup()
@@ -24,8 +27,8 @@ namespace SFA.DAS.EmployerIncentives.Functions.PaymentProcess.UnitTests
             _mockOrchestrationContext = new Mock<IDurableOrchestrationContext>();
             _mockOrchestrationContext.Setup(x => x.GetInput<CollectionPeriod>()).Returns(_collectionPeriod);
 
-            var legalEntities = _fixture.Create<List<long>>();
-            _mockOrchestrationContext.Setup(x => x.CallActivityAsync<List<long>>("GetPayableLegalEntities", _collectionPeriod)).ReturnsAsync(legalEntities);
+            _legalEntities = _fixture.CreateMany<long>(3).ToList();
+            _mockOrchestrationContext.Setup(x => x.CallActivityAsync<List<long>>("GetPayableLegalEntities", _collectionPeriod)).ReturnsAsync(_legalEntities);
 
             _orchestrator = new IncentivePaymentOrchestrator(Mock.Of<ILogger<IncentivePaymentOrchestrator>>());
         }
@@ -36,6 +39,27 @@ namespace SFA.DAS.EmployerIncentives.Functions.PaymentProcess.UnitTests
             await _orchestrator.RunOrchestrator(_mockOrchestrationContext.Object);
 
             _mockOrchestrationContext.Verify(x => x.CallActivityAsync<List<long>>("GetPayableLegalEntities", _collectionPeriod), Times.Once);
+        }
+
+        [Test]
+        public async Task Then_sub_orchestrator_is_called_to_calculate_payments_for_each_legal_entity()
+        {
+            await _orchestrator.RunOrchestrator(_mockOrchestrationContext.Object);
+
+            _mockOrchestrationContext.Verify(x => x.CallSubOrchestratorAsync<object>("CalculatePaymentsForAccountLegalEntityOrchestrator", null, It.Is<object>(y => VerifyInputMatchesAccountAndCollectionPeriod(y, _legalEntities[0]))), Times.Once);
+            _mockOrchestrationContext.Verify(x => x.CallSubOrchestratorAsync<object>("CalculatePaymentsForAccountLegalEntityOrchestrator", null, It.Is<object>(y => VerifyInputMatchesAccountAndCollectionPeriod(y, _legalEntities[1]))), Times.Once);
+            _mockOrchestrationContext.Verify(x => x.CallSubOrchestratorAsync<object>("CalculatePaymentsForAccountLegalEntityOrchestrator", null, It.Is<object>(y => VerifyInputMatchesAccountAndCollectionPeriod(y, _legalEntities[2]))), Times.Once);
+        }
+
+        private bool VerifyInputMatchesAccountAndCollectionPeriod(object functionInput, long accountLegalEntityId)
+        {
+            var accountLegalEntityAndCollectionPeriod = functionInput as AccountLegalEntityCollectionPeriod;
+            if (accountLegalEntityAndCollectionPeriod == null)
+            {
+                return false;
+            }
+
+            return accountLegalEntityAndCollectionPeriod.AccountLegalEntityId == accountLegalEntityId && accountLegalEntityAndCollectionPeriod.CollectionPeriod == _collectionPeriod;
         }
     }
 }
--- src/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess/IncentivePaymentOrchestrator.cs
+++ src/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess/IncentivePaymentOrchestrator.cs
@@ -24,10 +24,15 @@ namespace SFA.DAS.EmployerIncentives.Functions.PaymentsProcess
 
             var payableLegalEntities = await context.CallActivityAsync<List<long>>("GetPayableLegalEntities", collectionPeriod);
 
+            var calculatePaymentTasks = new List<Task>();
             foreach (var legalEntity in payableLegalEntities)
             {
                 _logger.LogInformation($"Request made to process payments for account legal entity {legalEntity}", new { collectionPeriod, legalEntity });
+                var calculatePaymentTask = context.CallSubOrchestratorAsync("CalculatePaymentsForAccountLegalEntityOrchestrator", new AccountLegalEntityCollectionPeriod { AccountLegalEntityId = legalEntity, CollectionPeriod = collectionPeriod });
+                calculatePaymentTasks.Add(calculatePaymentTask);
             }
+
+            await Task.WhenAll(calculatePaymentTasks);
 
             _logger.LogInformation($"Incentive Payment process completed for collection period {collectionPeriod}", new { collectionPeriod });
         }
