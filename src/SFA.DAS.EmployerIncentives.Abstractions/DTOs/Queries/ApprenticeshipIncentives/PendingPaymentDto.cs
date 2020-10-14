858628ed71edb166c28e6f4b98a996bfec0af36d
Michael Young <mick.young1981@googlemail.com>
2020-10-14 13:45:01 +0100

    Add new query repository

---
+++ src/SFA.DAS.EmployerIncentives.Abstractions/DTOs/Queries/ApprenticeshipIncentives/PendingPaymentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerIncentives.Abstractions.DTOs.Queries.ApprenticeshipIncentives
+{
+    public class PendingPaymentDto
+    {
+        public Guid Id { get; set; }
+        public short? PaymentPeriod { get; set; }
+        public short? PaymentYear { get; set; }
+        public long AccountLegalEntityId { get; set; }
+        public DateTime? PaymentMadeDate { get; set; }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Data.UnitTests/PendingPaymentQueryRepository/WhenGetPendingPaymentCalled.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Abstractions.DTOs.Queries.ApprenticeshipIncentives;
+using SFA.DAS.EmployerIncentives.Data.ApprenticeshipIncentives.Models;
+using SFA.DAS.EmployerIncentives.Data.Models;
+
+namespace SFA.DAS.EmployerIncentives.Data.UnitTests.PendingPaymentQueryRepository
+{
+    public class WhenGetPendingPaymentCalled
+    {
+        private EmployerIncentivesDbContext _context;
+        private Fixture _fixture;
+        private IQueryRepository<PendingPaymentDto> _sut;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+
+            var options = new DbContextOptionsBuilder<EmployerIncentivesDbContext>()
+                .UseInMemoryDatabase("EmployerIncentivesDbContext" + Guid.NewGuid()).Options;
+            _context = new EmployerIncentivesDbContext(options);
+
+            _sut = new ApprenticeshipIncentives.PendingPaymentQueryRepository(_context);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task Then_data_is_fetched_from_database()
+        {
+            // Arrange
+            var pendingPayment = _fixture.Create<PendingPayment>();
+            
+            _context.PendingPayments.Add(pendingPayment);
+            _context.SaveChanges();
+
+            // Act
+            var actual = await _sut.Get(x => x.Id == pendingPayment.Id);
+
+            //Assert
+            actual.Should().BeEquivalentTo(pendingPayment, opts => opts.ExcludingMissingMembers());
+        }
+
+        [Test]
+        public async Task Then_null_is_returned_when_the_application_is_not_found()
+        {
+            // Arrange
+            var allPendingPayments = _fixture.CreateMany<PendingPayment>(10).ToArray();
+            
+            _context.PendingPayments.AddRange(allPendingPayments);
+            _context.SaveChanges();
+
+            // Act
+            var actual = await _sut.Get(x => x.Id == Guid.NewGuid());
+
+            //Assert
+            actual.Should().BeNull();
+        }
+    }
+}
