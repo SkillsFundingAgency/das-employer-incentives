8fad24facacfb1e2714c6bbc5a0dd4ff10565c9e
Michael Young <mick.young1981@googlemail.com>
2020-10-14 14:07:16 +0100

    Added new query handler

---
+++ src/SFA.DAS.EmployerIncentives.Queries/ApprenticeshipIncentives/GetPendingPaymentsForAccountLegalEntity/GetPendingPaymentsForAccountLegalEntityRequest.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.EmployerIncentives.Abstractions.Queries;
+
+namespace SFA.DAS.EmployerIncentives.Queries.ApprenticeshipIncentives.GetPendingPaymentsForAccountLegalEntity
+{
+    public class GetPendingPaymentsForAccountLegalEntityRequest : IQuery
+    {
+        public long AccountLegalEntityId { get; }
+        public short CollectionPeriodYear { get; }
+        public short CollectionPeriodMonth { get; }
+
+        public GetPendingPaymentsForAccountLegalEntityRequest(long accountLegalEntityId, short collectionPeriodYear, short collectionPeriodMonth)
+        {
+            AccountLegalEntityId = accountLegalEntityId;
+            CollectionPeriodYear = collectionPeriodYear;
+            CollectionPeriodMonth = collectionPeriodMonth;
+        }
+    }
+}
---
+++ src/tests/SFA.DAS.EmployerIncentives.Queries.UnitTests/ApprenticeshipIncentives/WhenHandlingGetPendingPaymentsForAccountLegalEntityQuery.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Abstractions.DTOs.Queries.ApprenticeshipIncentives;
+using SFA.DAS.EmployerIncentives.Data;
+using SFA.DAS.EmployerIncentives.Queries.ApprenticeshipIncentives.GetPendingPaymentsForAccountLegalEntity;
+
+namespace SFA.DAS.EmployerIncentives.Queries.UnitTests.ApprenticeshipIncentives
+{
+    public class WhenHandlingGetPendingPaymentsForAccountLegalEntityQuery
+    {
+        private GetPendingPaymentsForAccountLegalEntityQueryHandler _sut;
+        private Mock<IQueryRepository<PendingPaymentDto>> _repositoryMock;
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+            _repositoryMock = new Mock<IQueryRepository<PendingPaymentDto>>();
+            _sut = new GetPendingPaymentsForAccountLegalEntityQueryHandler(_repositoryMock.Object);
+        }
+
+        [Test]
+        public async Task Then_data_is_fetched_via_query_repository()
+        {
+            //Arrange
+            var query = _fixture.Create<GetPendingPaymentsForAccountLegalEntityRequest>();
+            var data = _fixture.CreateMany<PendingPaymentDto>().ToList();
+            var expected = new GetPendingPaymentsForAccountLegalEntityResponse(data);
+
+            _repositoryMock.Setup(x => x.GetList(dto => dto.AccountLegalEntityId == query.AccountLegalEntityId && !dto.PaymentMadeDate.HasValue && (dto.PaymentYear < query.CollectionPeriodYear || (dto.PaymentYear == query.CollectionPeriodYear && dto.PaymentPeriod <= query.CollectionPeriodMonth)))).ReturnsAsync(data);
+
+            //Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            //Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+    }
+}
