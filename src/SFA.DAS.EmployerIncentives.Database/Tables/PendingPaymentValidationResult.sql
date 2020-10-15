12b73f299526130308d2957cf92c20448f6f628d
Paul Graham <paul.graham@coprime.co.uk>
2020-10-15 15:31:36 +0100

    added new paymentpendingvalidationresult table

---
+++ src/SFA.DAS.EmployerIncentives.Database/Tables/PendingPaymentValidationResult.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [incentives].[PendingPaymentValidationResult]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED,	
+	[PendingPaymentId] UNIQUEIDENTIFIER NOT NULL,
+    [ValidationStep] NVARCHAR(20) NOT NULL, 
+    [ValidationResult] BIT NOT NULL,
+    [CollectionPeriodMonth] TINYINT NOT NULL, 
+    [CollectionPeriodYear] SMALLINT NOT NULL, 
+    [CollectionDateUTC] DATETIME2 NOT NULL
+    CONSTRAINT FK_PendingPaymentId FOREIGN KEY (PendingPaymentId) REFERENCES [incentives].[PendingPayment](Id)
+)
+GO
+
+CREATE CLUSTERED INDEX [IX_PendingPaymentsValidationResult_PendingPaymentId] ON [incentives].[PendingPaymentValidationResult]
+(
+	[PendingPaymentId] 
+)
+GO
