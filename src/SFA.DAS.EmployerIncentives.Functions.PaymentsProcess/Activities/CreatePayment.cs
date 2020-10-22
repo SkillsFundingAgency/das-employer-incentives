9704833018981f87ad24ddba5a73067e41117929
Michael Young <mick.young1981@googlemail.com>
2020-10-22 11:57:15 +0100

    Added activity function.

---
+++ src/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess/Activities/CreatePayment.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+using SFA.DAS.EmployerIncentives.Commands.ApprenticeshipIncentive.CreatePayment;
+
+namespace SFA.DAS.EmployerIncentives.Functions.PaymentsProcess.Activities
+{
+    public class CreatePayment
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private ILogger<CreatePayment> _logger;
+
+        public CreatePayment(ICommandDispatcher commandDispatcher, ILogger<CreatePayment> logger)
+        {
+            _commandDispatcher = commandDispatcher;
+            _logger = logger;
+        }
+
+        [FunctionName("CreatePayment")]
+        public async Task Create([ActivityTrigger] CreatePaymentInput input)
+        {
+            _logger.LogInformation($"Creating Payment for apprenticeship incentive id {input.ApprenticeshipIncentiveId}, pending payment id {input.PendingPaymentId}, collection period {input.CollectionPeriod}", new { input.ApprenticeshipIncentiveId, input.PendingPaymentId, input.CollectionPeriod });
+            await _commandDispatcher.Send(new CreatePaymentCommand(input.ApprenticeshipIncentiveId, input.PendingPaymentId, input.CollectionPeriod.Year, input.CollectionPeriod.Month));
+            _logger.LogInformation($"Created Payment for apprenticeship incentive id {input.ApprenticeshipIncentiveId}, pending payment id {input.PendingPaymentId}, collection period {input.CollectionPeriod}", new { input.ApprenticeshipIncentiveId, input.PendingPaymentId, input.CollectionPeriod });
+        }
+    }
+}
