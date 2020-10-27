ea1996bf3ed1212be71578fef11018e3e5cae3cd
Paul Graham <paul.graham@coprime.co.uk>
2020-10-27 16:54:33 +0000

    added a basic function to get next collection period

---
+++ src/SFA.DAS.EmployerIncentives.Database/Views/PendingPaymentsValidationStepFailure.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [incentives].[PendingPaymentsValidationStepFailure]
+AS
+SELECT 1 AS TEST
+--SELECT 
+--	VR.ValidationStep,
+--	SUM(CASE WHEN ValidationResult = 0 THEN 1 ELSE 0 END) AS Failures
+--FROM incentives.PendingPaymentValidationResult VR
+--JOIN incentives.GetNextCollectionPeriod(GETDATE()) CP 
+--ON VR.CollectionPeriodMonth = CP.CalendarMonth AND VR.CollectionPeriodYear = CP.CalendarYear
+--GROUP BY VR.ValidationStep
+GO
