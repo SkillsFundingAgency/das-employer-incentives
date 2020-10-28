7bb35e20dc79fa18295da53ea0880f2c8a1af725
Paul Graham <paul.graham@coprime.co.uk>
2020-10-28 08:18:15 +0000

    added views for dashboard

---
+++ src/SFA.DAS.EmployerIncentives.Database/Views/PendingPaymentsValidationStatus.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [incentives].[PendingPaymentsValidationStatus]
+AS
+SELECT 
+	VR.PendingPaymentId,
+	CASE WHEN COUNT(*) = SUM(CASE WHEN VR.Result = 1 THEN 1 ELSE 0 END) THEN 1
+	ELSE 0
+	END AS ValidStatus
+FROM incentives.PendingPaymentValidationResult VR
+JOIN incentives.GetNextCollectionPeriod(GETDATE()) CP 
+ON VR.CollectionPeriodMonth = CP.CalendarMonth AND VR.CollectionPeriodYear = CP.CalendarYear
+GROUP BY VR.PendingPaymentId
+GO
