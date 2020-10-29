100e190c67f1ef708f23d88926dec94058c3ad21
Ruslan <ruslanss@gmail.com>
2020-10-29 14:51:49 +0000

    .

--- src/SFA.DAS.EmployerIncentives.Functions.TestConsole/NServiceBusConsole.cs
+++ src/SFA.DAS.EmployerIncentives.Functions.TestConsole/NServiceBusConsole.cs
@@ -24,17 +24,6 @@ namespace SFA.DAS.EmployerIncentives.Functions.TestConsole
                 .UseTransport<LearningTransport>()
                 .StorageDirectory(storageDirectory);
 
-            /* var endpointConfiguration = new EndpointConfiguration("SFA.DAS.EmployerIncentives.Functions.DomainMessageHandlers")
-                   .UseMessageConventions()
-                   .UseNewtonsoftJsonSerializer()
-                   .UseOutbox(true)
-                   .UseSqlServerPersistence(() => new SqlConnection("Data Source =.; Initial Catalog = SFA.DAS.EmployerIncentives.Database; Integrated Security = True; Pooling = False; Connect Timeout = 30"))
-                   .UseUnitOfWork();
-
-             endpointConfiguration
-                .UseTransport<AzureServiceBusTransport>()
-                .ConnectionString("Endpoint=sb://das-at-shared-ns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Xq9YUB/8z22wfmnT7Gi3081gUxbbGzaZqArdkXY16cY=");
-            */
             var endpointInstance = await Endpoint.Start(endpointConfiguration)
                 .ConfigureAwait(false);
 
@@ -42,16 +31,6 @@ namespace SFA.DAS.EmployerIncentives.Functions.TestConsole
 
             do
             {
-                //var message = new AddedLegalEntityEvent
-                //{
-                //    AccountId = 2,
-                //    AccountLegalEntityId = 2,
-                //    LegalEntityId = 3,
-                //    OrganisationName = "Org name"
-                //};
-
-                //await endpointInstance.Publish(message);
-
                 var message2 = new Commands.Types.ApprenticeshipIncentive.CreateIncentiveCommand(1, 2, new List<CreateIncentiveCommand.IncentiveApprenticeship>());
                 await endpointInstance.Send(message2);
 
