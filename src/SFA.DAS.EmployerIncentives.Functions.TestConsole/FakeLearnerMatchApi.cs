eb6f5fbf2d210fd85f81b7a3a6c62f25bba9ebcf
Ruslan <ruslanss@gmail.com>
2020-11-04 17:34:55 +0000

    Add FakeLearnerMatchApi to enable F5 debugging of Payments Functions

---
+++ src/SFA.DAS.EmployerIncentives.Functions.TestConsole/FakeLearnerMatchApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WireMock.Server;
+
+namespace SFA.DAS.EmployerIncentives.Functions.TestConsole
+{
+#pragma warning disable S3881 // "IDisposable" should be implemented correctly
+    public class FakeLearnerMatchApi : IDisposable
+    {
+        private readonly WireMockServer _server;
+        public FakeLearnerMatchApi(WireMockServer server)
+        {
+            _server = server;
+        }
+
+        public void Dispose()
+        {
+            if (_server.IsStarted)
+            {
+                _server.Stop();
+            }
+        }
+    }
+#pragma warning restore S3881 // "IDisposable" should be implemented correctly
+}
