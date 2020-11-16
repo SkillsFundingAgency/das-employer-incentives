4cfa2d72c7138e3491765869323ac5a98704f647
Ruslan <ruslanss@gmail.com>
2020-11-16 13:13:44 +0000

    Revert unwanted changes

--- src/SFA.DAS.EmployerIncentives.Data.UnitTests/TestHelpers/SqlDatabase.cs
+++ src/SFA.DAS.EmployerIncentives.Data.UnitTests/TestHelpers/SqlDatabase.cs
@@ -23,8 +23,7 @@ namespace SFA.DAS.EmployerIncentives.Data.UnitTests.TestHelpers
 
             var dacpacFileLocation =
                 Path.Combine(
-                    Directory.GetCurrentDirectory().Substring(0,
-                        Directory.GetCurrentDirectory().IndexOf("src", StringComparison.Ordinal)),
+                    Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().IndexOf("src", StringComparison.Ordinal)),
                     $"src\\SFA.DAS.EmployerIncentives.Database\\bin\\{environment}\\SFA.DAS.EmployerIncentives.Database.dacpac");
 
             if (!File.Exists(dacpacFileLocation))
@@ -38,8 +37,7 @@ namespace SFA.DAS.EmployerIncentives.Data.UnitTests.TestHelpers
         private void CreateTestDatabase()
         {
             DatabaseInfo.SetDatabaseName(Guid.NewGuid().ToString());
-            DatabaseInfo.SetConnectionString(
-                @$"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog={DatabaseInfo.DatabaseName};Integrated Security=True;Pooling=False;Connect Timeout=30");
+            DatabaseInfo.SetConnectionString(@$"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog={DatabaseInfo.DatabaseName};Integrated Security=True;Pooling=False;Connect Timeout=30");
 
             Publish();
 
@@ -66,7 +64,6 @@ namespace SFA.DAS.EmployerIncentives.Data.UnitTests.TestHelpers
                                 files.Add((string)reader["filename"]);
                             }
                         }
-
                         cmd.CommandText = $"ALTER DATABASE [{dbName}] SET OFFLINE";
                         cmd.ExecuteNonQuery();
                         cmd.CommandText = $"EXEC sp_detach_db '{dbName}', 'true';";
@@ -74,13 +71,9 @@ namespace SFA.DAS.EmployerIncentives.Data.UnitTests.TestHelpers
                         dbConn.Close();
                     }
                 }
-
                 files.ForEach(DeleteFile);
             }
-            catch
-            {
-                /*ignored*/
-            }
+            catch { /*ignored*/ }
         }
 
         private static void DeleteFile(string file)
@@ -89,10 +82,7 @@ namespace SFA.DAS.EmployerIncentives.Data.UnitTests.TestHelpers
             {
                 File.Delete(file);
             }
-            catch
-            {
-                /*ignored*/
-            }
+            catch { /*ignored*/ }
         }
 
         private void Publish()
@@ -131,28 +121,6 @@ namespace SFA.DAS.EmployerIncentives.Data.UnitTests.TestHelpers
             }
 
             _isDisposed = true;
-        }
-
-        /// <summary>
-        /// Database data clear-down script. Ensure tables containing reference data are excluded below.
-        /// </summary>
-        public void ClearDown()
-        {
-            const string sql = @"
-                EXEC sys.sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL';
-                EXEC sys.sp_msforeachtable 'IF OBJECT_ID(''?'') NOT IN (
-                     ISNULL(OBJECT_ID(''[dbo].[__RefactorLog]''),0)
-                    ,ISNULL(OBJECT_ID(''[incentives].[CollectionCalendar]''),0)
-                )
-                BEGIN SET QUOTED_IDENTIFIER ON; DELETE FROM ?; END'
-                EXEC sys.sp_msforeachtable 'ALTER TABLE ? CHECK CONSTRAINT ALL'
-                ";
-
-            using var dbConn = new SqlConnection(DatabaseInfo.ConnectionString);
-            using var cmd = new SqlCommand(sql, dbConn);
-            dbConn.Open();
-            cmd.ExecuteNonQuery();
-            dbConn.Close();
         }
     }
 }
