e9d0a5956f4b93d638924f53c403fd036c523ee0
Michael Young <mick.young1981@googlemail.com>
2020-11-19 12:04:25 +0000

    Fixed acceptance test

--- src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Steps/ApprenticeshipIncentiveCreatedSteps.cs
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Steps/ApprenticeshipIncentiveCreatedSteps.cs
@@ -58,6 +58,7 @@ namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Steps
                 .With(p => p.ApprenticeshipId, _apprenticeshipsModels.First().ApprenticeshipId)
                 .With(p => p.PlannedStartDate, DateTime.Today.AddDays(1))
                 .With(p => p.DateOfBirth, DateTime.Today.AddYears(-20))
+                .With(p => p.ActualStartDate, (DateTime?)null)
                 .Create();
 
             _pendingPayment = _fixture.Build<PendingPayment>()
