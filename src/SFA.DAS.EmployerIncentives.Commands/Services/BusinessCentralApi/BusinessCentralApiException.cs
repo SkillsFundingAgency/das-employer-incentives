4ff955cefacf488fc23dfc125d54837325c51f1b
Paul Graham <paul.graham@coprime.co.uk>
2020-11-27 15:53:26 +0000

    updated BC excpetion so it is serializable

--- src/SFA.DAS.EmployerIncentives.Commands/Services/BusinessCentralApi/BusinessCentralApiException.cs
+++ src/SFA.DAS.EmployerIncentives.Commands/Services/BusinessCentralApi/BusinessCentralApiException.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
+using System.Runtime.Serialization;
 
 namespace SFA.DAS.EmployerIncentives.Commands.Services.BusinessCentralApi
 {
+    [Serializable]
     public class BusinessCentralApiException : ApplicationException
     {
-        public HttpStatusCode HttpStatusCode { get; }
+        public HttpStatusCode? HttpStatusCode { get; }
 
         public BusinessCentralApiException(string message, HttpStatusCode httpStatusCode) : base(message)
         {
             HttpStatusCode = httpStatusCode;
+        }
+
+        protected BusinessCentralApiException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            this.HttpStatusCode = (HttpStatusCode?)info.GetValue("HttpStatusCode", typeof(HttpStatusCode));
         }
     }
 }
