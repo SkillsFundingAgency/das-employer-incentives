97d4dc1e85958e6a297d9e53fc9b76986ac8e3f9
Paul Graham <paul.graham@coprime.co.uk>
2020-12-07 11:29:02 +0000

    Revert "reverted date changes so it's now back to json date format"
    This reverts commit f8a5ed76bcde074b880e56520419e07a98a98e59.

--- src/SFA.DAS.EmployerIncentives.Commands/Services/BusinessCentralApi/FundingStream.cs
+++ src/SFA.DAS.EmployerIncentives.Commands/Services/BusinessCentralApi/FundingStream.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.EmployerIncentives.Commands.Services.BusinessCentralApi
     public class FundingStream
     {
         public string Code { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
     }
 }
