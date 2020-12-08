b8f5047701b755d52312b811bfd8b7e548e06eb8
David Steele <db.steele@gmail.com>
2020-12-08 10:24:41 +0000

    Updated the BC payment %

--- src/SFA.DAS.EmployerIncentives.Database/Views/BusinessPaymentsDashboard.sql
+++ src/SFA.DAS.EmployerIncentives.Database/Views/BusinessPaymentsDashboard.sql
@@ -6,9 +6,9 @@ SELECT
   count(pp.Id) as [Num Earnings],
   SUM(lastval.FailedValidation) as [Num EI Validation Failed],
   SUM(CASE WHEN pp.paymentmadedate is not null THEN 1 else 0 END) as [Num EI Payment Records],
-  (SUM(lastval.FailedValidation)+cast(SUM(CASE WHEN pp.paymentmadedate is not null THEN 1 else 0 END) as float)*100)/nullif(count(pp.Id),0) as [% Earnings Handled],
+  ((SUM(lastval.FailedValidation)+cast(SUM(CASE WHEN pp.paymentmadedate is not null THEN 1 else 0 END) as float))/nullif(count(pp.Id),0))*100 as [% Earnings Handled],
   SUM(CASE WHEN p.PaidDate is not null THEN 1 else 0 END) as [Num BC Payments Sent], 
-  isnull(SUM(CASE WHEN p.PaidDate is not null THEN 1 else 0 END)/nullif((cast(count(pp.Id) as float)),0)*100,0) as [% Earning to BC Payments],
+  isnull((SUM(CASE WHEN p.PaidDate is not null THEN 1 else 0 END)/nullif((cast(SUM(CASE WHEN pp.paymentmadedate is not null THEN 1 else 0 END) as float)),0))*100,0) as [% Valid earning to BC Payments],
   count(distinct p.AccountId) as [Num EI Accounts expecting payment],
   count(distinct CASE WHEN p.PaidDate is null THEN p.AccountId else 0 END) as [Num Accounts with missing BC payments],
   count(distinct a.VrfVendorId) as [Num Vendors expecting payment],
@@ -18,10 +18,9 @@ SELECT
   FORMAT(sum(pp.amount),'C','en-gb') as [EI Pending Payments Value], 
   FORMAT(sum(CASE WHEN lastval.FailedValidation = 1 then pp.Amount else 0 END),'C','en-gb') as [EI Validation Failed Value],
   FORMAT(sum(case when pp.paymentmadedate is not null then pp.amount else 0 end),'C','en-gb') as [EI Payments Made Value],
-  FORMAT(sum(case when p.amount >= 0 then p.amount else 0 end),'C','en-gb') as [EI Positive Payments Value], 
+  FORMAT(sum(case when p.amount >= 0 then p.amount else 0 end),'C','en-gb') as [EI Postive Payments Value], 
   FORMAT(sum(case when p.amount < 0 then p.amount else 0 end),'C','en-gb') as [EI Negative Payments Value], 
   FORMAT(SUM(CASE WHEN p.PaidDate is not null THEN p.Amount else 0 END),'C','en-gb') as [BC Payments Sent Value]
-
 FROM
   [incentives].[PendingPayment] pp
   left join [incentives].[Payment] p on p.PendingPaymentId = pp.Id
