164635c0ba85dee2bb7bd25220ed6d7483d3f7cf
Kevin <kevin@stephek.co.uk>
2020-12-09 16:20:38 +0000

    EI-676-more-tests

---
+++ src/SFA.DAS.EmployerIncentives.Domain.UnitTests/IncentiveApplicationTests/WhenAnApprenticeshipIsWithdrawn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.EmployerIncentives.Domain.Factories;
+using SFA.DAS.EmployerIncentives.Domain.IncentiveApplications;
+using SFA.DAS.EmployerIncentives.Domain.IncentiveApplications.Events;
+using SFA.DAS.EmployerIncentives.Domain.IncentiveApplications.Models;
+using SFA.DAS.EmployerIncentives.Domain.ValueObjects;
+
+namespace SFA.DAS.EmployerIncentives.Domain.UnitTests.IncentiveApplicationTests
+{
+    public class WhenAnApprenticeshipIsWithdrawn
+    {
+        private Fixture _fixture;
+        private IncentiveApplication _sut;
+        private IncentiveApplicationModel _sutModel;
+     
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+
+            _sutModel = _fixture
+                .Build<IncentiveApplicationModel>()
+                .With(i => i.ApprenticeshipModels,
+                new List<ApprenticeshipModel>()
+                {
+                    _fixture.Create<ApprenticeshipModel>()
+                })
+                .Create();
+
+            _sut = new IncentiveApplicationFactory().GetExisting(_sutModel.Id, _sutModel);
+        }
+
+        [Test]
+        public void Then_apprenticeship_is_marked_as_employer_withdrawn_when_an_employer_withdraws_the_application()
+        {
+            // Arrange
+            var serviceRequest = _fixture.Create<ServiceRequest>();
+            var apprenticeship = _sut.Apprenticeships.Single();
+            apprenticeship.WithdrawnByEmployer.Should().BeFalse();
+
+            // Act
+            _sut.EmployerWithdrawn(apprenticeship, serviceRequest);
+
+            // Assert
+            apprenticeship.WithdrawnByEmployer.Should().BeTrue();
+        }
+
+        [Test]
+        public void Then_an_EmployerWithdrawn_event_is_raised_when_an_employer_withdraws_the_application()
+        {
+            // Arrange
+            var serviceRequest = _fixture.Create<ServiceRequest>();
+            var apprenticeship = _sut.Apprenticeships.Single();
+      
+            // Act
+            _sut.EmployerWithdrawn(apprenticeship, serviceRequest);
+
+            // Assert
+            var raisedEvent = _sut.FlushEvents().Single() as EmployerWithdrawn;
+            raisedEvent.AccountId.Should().Be(_sut.AccountId);
+            raisedEvent.AccountLegalEntityId.Should().Be(_sut.AccountLegalEntityId);
+            raisedEvent.Model.Should().Be(apprenticeship.GetModel());
+            raisedEvent.ServiceRequest.Should().Be(serviceRequest);
+        }
+    }
+}
---
+++ src/tests/SFA.DAS.EmployerIncentives.Commands.UnitTests/Withdrawls/EmployerWithdrawl/Validators/WhenValidatingEmployerWithdrawlCommand.cs
@@ -0,0 +1,60 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Commands.Withdrawls.EmployerWithdrawl;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Commands.UnitTests.Withdrawls.EmployerWithdrawl.Validators
+{
+    public class WhenValidatingEmployerWithdrawlCommand
+    {
+        private EmployerWithdrawlCommandValidator _sut;
+        
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+
+            _sut = new EmployerWithdrawlCommandValidator();
+        }
+
+        [Test]
+        public async Task Then_the_command_is_invalid_when_the_AccountLegalEntityId_has_a_default_value()
+        {
+            //Arrange
+            var command = new EmployerWithdrawlCommand(
+                default, 
+                _fixture.Create<long>(), 
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<DateTime>());
+
+            //Act
+            var result = await _sut.Validate(command);
+
+            //Assert
+            result.ValidationDictionary.Count.Should().Be(1);
+        }
+
+        [Test]
+        public async Task Then_the_command_is_invalid_when_the_ULN_has_a_default_value()
+        {
+            //Arrange
+            var command = new EmployerWithdrawlCommand(                
+                _fixture.Create<long>(),
+                default,
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<DateTime>());
+
+            //Act
+            var result = await _sut.Validate(command);
+
+            //Assert
+            result.ValidationDictionary.Count.Should().Be(1);
+        }
+    }
+}
