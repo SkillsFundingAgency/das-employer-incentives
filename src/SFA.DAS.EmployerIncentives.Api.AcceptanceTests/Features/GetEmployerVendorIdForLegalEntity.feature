82e821a9130613e769c2f8186ac394bb552bb1b0
Dave Read <david.read@digital.education.gov.uk>
2020-12-21 12:56:37 +0000

    EI-733 acceptance tests

---
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Features/GetEmployerVendorIdForLegalEntity.feature
@@ -0,0 +1,18 @@
+﻿@database
+@api
+@accountApi
+
+Feature: GetEmployerVendorIdForLegalEntity
+	In order to ensure that the end to end payment process works effectively
+	As a service owner
+	I want to check whether an account legal entity has an associated vendor id
+
+Scenario: Account legal entity has a vendor id assigned
+Given a legal entity exists within an account
+And the legal entity has a vendor id assigned
+Then the vendor id associated with the account legal entity is returned
+
+Scenario: Account legal entity does not have a vendor id assigned
+Given a legal entity exists within an account
+And the legal entity does not have a vendor id assigned
+Then no vendor id is returned
---
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Steps/GetEmployerVendorIdForLegalEntitySteps.cs
@@ -0,0 +1,76 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using SFA.DAS.EmployerIncentives.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Steps
+{
+    [Binding]
+    [Scope(Feature = "GetEmployerVendorIdForLegalEntity")]
+    public class GetEmployerVendorIdForLegalEntitySteps : StepsBase
+    {
+        private Account _account;
+        private string _hashedLegalEntityId;
+        private string _vendorId;
+
+        public GetEmployerVendorIdForLegalEntitySteps(TestContext testContext) : base(testContext)
+        {
+           
+        }
+
+        [Given(@"a legal entity exists within an account")]
+        public void GivenALegalEntityExistsWithinAnAccount()
+        {
+            _hashedLegalEntityId = "PVP9DM";
+            _account = TestContext.TestData.GetOrCreate<Account>("Account");
+            _account.HashedLegalEntityId = _hashedLegalEntityId;            
+        }
+
+        [Given(@"the legal entity has a vendor id assigned")]
+        public void GivenTheLegalEntityHasAVendorIdAssigned()
+        {
+            _vendorId = "ABC123";
+            _account.VrfVendorId = _vendorId;
+            DataAccess.SetupAccount(_account);
+        }
+
+        [Then(@"the vendor id associated with the account legal entity is returned")]
+        public async Task ThenTheVendorIdAssociatedWithTheAccountLegalEntityIsReturned()
+        {
+            var url = $"/legalentities/{_hashedLegalEntityId}/employervendorid";
+
+            var (status, data) =
+                await EmployerIncentiveApi.Client.GetValueAsync<string>(url);
+
+            status.Should().Be(HttpStatusCode.OK);
+
+            data.Should().Be(_vendorId);
+        }
+
+        [Given(@"the legal entity does not have a vendor id assigned")]
+        public void GivenTheLegalEntityDoesNotHaveAVendorIdAssigned()
+        {
+            _account.VrfVendorId = null;
+            DataAccess.SetupAccount(_account);
+        }
+
+        [Then(@"no vendor id is returned")]
+        public async Task ThenNoVendorIdIsReturned()
+        {
+            var url = $"/legalentities/{_hashedLegalEntityId}/employervendorid";
+
+            var (status, data) =
+                await EmployerIncentiveApi.Client.GetValueAsync<string>(url);
+
+            status.Should().Be(HttpStatusCode.NotFound);
+
+            data.Should().BeNull();
+        }
+
+    }
+}
