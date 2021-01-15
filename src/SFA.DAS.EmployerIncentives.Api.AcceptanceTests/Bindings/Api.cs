ef6f9fe6d681bd4d26f8f46fd34de8992f436b9a
Kevin <kevin@stephek.co.uk>
2021-01-15 00:51:17 +0000

    EI-443-test-using-free-port

--- src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Bindings/Api.cs
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Bindings/Api.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using SFA.DAS.EmployerIncentives.Abstractions.Commands;
 using SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Hooks;
+using System;
+using System.Linq;
+using System.Net.NetworkInformation;
 using TechTalk.SpecFlow;
 
 namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Bindings
@@ -27,11 +30,26 @@ namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Bindings
 
             var webApi = new TestWebApi(_context, eventsHook, commandsHook);
             var options = new WebApplicationFactoryClientOptions
-            {
-                BaseAddress = new System.Uri(@"https://localhost:5001")
+            {                
+                BaseAddress = new System.Uri($"https://localhost:{GetAvailablePort(5001)}")
             };
             _context.EmployerIncentivesWebApiFactory = webApi;
             _context.EmployerIncentiveApi = new EmployerIncentiveApi(webApi.CreateClient(options));
+        }
+
+        public int GetAvailablePort(int startingPort)
+        {
+            if (startingPort > ushort.MaxValue) throw new ArgumentException($"Can't be greater than {ushort.MaxValue}", nameof(startingPort));
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            var connectionsEndpoints = ipGlobalProperties.GetActiveTcpConnections().Select(c => c.LocalEndPoint);
+            var tcpListenersEndpoints = ipGlobalProperties.GetActiveTcpListeners();
+            var udpListenersEndpoints = ipGlobalProperties.GetActiveUdpListeners();
+            var portsInUse = connectionsEndpoints.Concat(tcpListenersEndpoints)
+                                                 .Concat(udpListenersEndpoints)
+                                                 .Select(e => e.Port);
+
+            return Enumerable.Range(startingPort, ushort.MaxValue - startingPort + 1).Except(portsInUse).FirstOrDefault();
         }
     }
 }
