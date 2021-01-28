1feeae40d748d7bf7c35aa20cade0d1a5468be03
David Steele <db.steele@gmail.com>
2021-01-28 15:42:37 +0000

    Initial version of the validation by period

---
+++ src/SFA.DAS.EmployerIncentives.Database/Views/BusinessValidationRulesByPeriodDashboard.sql
@@ -0,0 +1,44 @@
+﻿CREATE VIEW [dbo].[BusinessValidationRulesByPeriodDashboard]
+	AS
+
+with MonthEndRuntime as (
+  select min(createddateutc) FirstValidation, max(createddateutc) LastValidation, PeriodNumber, PaymentYear
+			from	[incentives].[PendingPaymentValidationResult]
+			group by PeriodNumber, PaymentYear
+
+),
+PendingPaymentValidations as (
+	select	PendingPaymentId, 
+			iif(step='HasIlrSubmission',1,0) as HasIlrSubmission,
+			iif(step='HasLearningRecord',1,0) as HasLearningRecord,
+			iif(step='IsInLearning',1,0) as IsInLearning,
+			iif(step='HasDaysInLearning',1,0) as HasDaysInLearning,
+			iif(step='HasNoDataLocks',1,0) as HasNoDataLocks,
+			iif(step='HasBankDetails',1,0) as HasBankDetails,
+			iif(step='PaymentsNotPaused',1,0) as PaymentsNotPaused,
+			result,
+			PeriodNumber, 
+			PaymentYear
+	from	[incentives].[PendingPaymentValidationResult] ppvr
+	where	ppvr.PeriodNumber = (select max(PeriodNumber) from MonthEndRuntime)
+	and		ppvr.PaymentYear = (select max(PaymentYear) from MonthEndRuntime)
+	group by PendingPaymentId, step, result, PeriodNumber, PaymentYear
+)
+select count(distinct pendingpaymentId) as CountOfPayments, 
+		HasLearningRecord, 
+		IsInLearning, 
+		HasDaysInLearning, 
+		HasNoDataLocks, 
+		HasBankDetails, 
+		PaymentsNotPaused
+from PendingPaymentValidations
+group by HasIlrSubmission, 
+		HasLearningRecord, 
+		IsInLearning, 
+		HasDaysInLearning, 
+		HasNoDataLocks, 
+		HasBankDetails, 
+		PaymentsNotPaused, 
+		Result
+
+
