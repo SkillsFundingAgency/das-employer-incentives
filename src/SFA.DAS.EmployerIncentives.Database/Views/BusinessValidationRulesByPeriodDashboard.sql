20fd3e63bb72b9643ffd1ebc5ec6f7335882b968
David Steele <db.steele@gmail.com>
2021-02-02 10:32:51 +0000

    Updated validation query to return correct results.

--- src/SFA.DAS.EmployerIncentives.Database/Views/BusinessValidationRulesByPeriodDashboard.sql
+++ src/SFA.DAS.EmployerIncentives.Database/Views/BusinessValidationRulesByPeriodDashboard.sql
@@ -3,20 +3,20 @@
 
 with PendingPaymentValidations as (
 	select	PendingPaymentId, 
-			iif(step='HasIlrSubmission',1,0) as HasIlrSubmission,
-			iif(step='HasLearningRecord',1,0) as HasLearningRecord,
-			iif(step='IsInLearning',1,0) as IsInLearning,
-			iif(step='HasDaysInLearning',1,0) as HasDaysInLearning,
-			iif(step='HasNoDataLocks',1,0) as HasNoDataLocks,
-			iif(step='HasBankDetails',1,0) as HasBankDetails,
-			iif(step='PaymentsNotPaused',1,0) as PaymentsNotPaused,
-			result,
+			max(iif(step='HasIlrSubmission',1,0)) as HasIlrSubmission,
+			max(iif(step='HasLearningRecord',1,0)) as HasLearningRecord,
+			max(iif(step='IsInLearning',1,0)) as IsInLearning,
+			max(iif(step='HasDaysInLearning',1,0)) as HasDaysInLearning,
+			max(iif(step='HasNoDataLocks',1,0)) as HasNoDataLocks,
+			max(iif(step='HasBankDetails',1,0)) as HasBankDetails,
+			max(iif(step='PaymentsNotPaused',1,0)) as PaymentsNotPaused,
+			Result,
 			PeriodNumber, 
 			PaymentYear
 	from	[incentives].[PendingPaymentValidationResult] ppvr
 	where	ppvr.PeriodNumber = (select max(PeriodNumber) from [BusinessGetMonthEndRuntimes])
 	and		ppvr.PaymentYear = (select max(PaymentYear) from [BusinessGetMonthEndRuntimes])
-	group by PendingPaymentId, step, result, PeriodNumber, PaymentYear
+	group by PendingPaymentId, result, PeriodNumber, PaymentYear
 )
 select count(distinct pendingpaymentId) as CountOfPayments, 
 		HasLearningRecord, 
