c051b79d9bfa591c0e3a111c73eb0485ba97b2a7
Kevin <kevin@stephek.co.uk>
2021-02-11 15:23:58 +0000

    EI-751-undo-previous-test

--- src/SFA.DAS.EmployerIncentives.Commands/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.EmployerIncentives.Commands/ServiceCollectionExtensions.cs
@@ -160,7 +160,8 @@ namespace SFA.DAS.EmployerIncentives.Commands
                 .AddSingleton(typeof(IValidator<EarningsResilienceApplicationsCheckCommand>), new NullValidator())
                 .AddSingleton(typeof(IValidator<EarningsResilienceIncentivesCheckCommand>), new NullValidator())
                 .AddSingleton(typeof(IValidator<SendPaymentRequestsCommand>), new NullValidator())
-                .AddSingleton(typeof(IValidator<WithdrawCommand>), new NullValidator())                
+                .AddSingleton(typeof(IValidator<WithdrawCommand>), new NullValidator())
+                .AddSingleton(typeof(IValidator<UpdateVrfCaseStatusForAccountCommand>), new NullValidator())
                 ;
 
             return serviceCollection;
