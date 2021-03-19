984a5b1d9bd143243c23bd57e668cd57bcc55adf
Michael Young <mick.young1981@googlemail.com>
2021-03-19 09:49:34 +0000

    Add command handler to set the active period to in progress.

---
+++ src/SFA.DAS.EmployerIncentives.Commands/CollectionCalendar/SetActivePeriodToInProgress/SetActivePeriodToInProgressCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+using SFA.DAS.EmployerIncentives.Domain.Interfaces;
+
+namespace SFA.DAS.EmployerIncentives.Commands.CollectionCalendar.SetActivePeriodToInProgress
+{
+    public class SetActivePeriodToInProgressCommandHandler : ICommandHandler<SetActivePeriodToInProgressCommand>
+    {
+        private readonly ICollectionCalendarService _collectionCalendarService;
+
+        public SetActivePeriodToInProgressCommandHandler(ICollectionCalendarService collectionCalendarService)
+        {
+            _collectionCalendarService = collectionCalendarService;
+        }
+
+        public async Task Handle(SetActivePeriodToInProgressCommand command, CancellationToken cancellationToken = default)
+        {
+            var collectionCalendar = await _collectionCalendarService.Get();
+            var activePeriod = collectionCalendar.GetActivePeriod();
+            activePeriod.SetPeriodEndInProgress(true);
+            await _collectionCalendarService.Save(collectionCalendar);
+        }
+    }
+}
---
+++ src/tests/SFA.DAS.EmployerIncentives.Commands.UnitTests/CollectionCalendar/Handlers/WhenHandlingSetActivePeriodToInProgressCommand.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using SFA.DAS.EmployerIncentives.Commands.CollectionCalendar.SetActivePeriodToInProgress;
+using SFA.DAS.EmployerIncentives.Domain.Interfaces;
+
+namespace SFA.DAS.EmployerIncentives.Commands.UnitTests.CollectionCalendar.Handlers
+{
+    [TestFixture]
+    public class WhenHandlingSetActivePeriodToInProgressCommand
+    {
+        private SetActivePeriodToInProgressCommandHandler _sut;
+        private Mock<ICollectionCalendarService> _service;
+        private Domain.ValueObjects.CollectionPeriod _activeCollectionPeriod;
+        private Domain.ValueObjects.CollectionCalendar _collectionCalendar;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _service = new Mock<ICollectionCalendarService>();
+            _sut = new SetActivePeriodToInProgressCommandHandler(_service.Object);
+
+            _activeCollectionPeriod = new Domain.ValueObjects.CollectionPeriod(2, 2021);
+            _activeCollectionPeriod.SetActive(true);
+
+            var calendarPeriods = new List<Domain.ValueObjects.CollectionPeriod>
+            {
+                new Domain.ValueObjects.CollectionPeriod(1, 2021),
+                _activeCollectionPeriod,
+                new Domain.ValueObjects.CollectionPeriod(3, 2021)
+            };
+
+            _collectionCalendar = new Domain.ValueObjects.CollectionCalendar(calendarPeriods);
+            _service.Setup(x => x.Get()).ReturnsAsync(_collectionCalendar);
+        }
+
+        [Test]
+        public async Task Then_the_active_period_is_set_to_in_progress()
+        {
+            // Arrange
+            var command = new SetActivePeriodToInProgressCommand();
+
+            // Act
+            await _sut.Handle(command);
+
+            // Assert
+            _activeCollectionPeriod.PeriodEndInProgress.Should().BeTrue();
+            _service.Verify(x => x.Save(_collectionCalendar), Times.Once);
+        }
+    }
+}
