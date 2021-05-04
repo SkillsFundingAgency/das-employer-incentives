47092dbea358676a619ebf5ceb78fbf4f328de3d
Kevin <kevin@stephek.co.uk>
2021-05-04 17:11:51 +0100

    EI-1078-extra-tests

--- src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Features/IncentivePhase.feature.cs
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Features/IncentivePhase.feature.cs
@@ -100,6 +100,8 @@ namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Features
         [NUnit.Framework.TestCaseAttribute("2021-03-31", "2021-05-31", "Phase1_1", null)]
         [NUnit.Framework.TestCaseAttribute("2021-04-01", "2021-05-31", "NotSet", null)]
         [NUnit.Framework.TestCaseAttribute("2021-04-01", "2021-06-01", "Phase2_0", null)]
+        [NUnit.Framework.TestCaseAttribute("2021-04-01", "2021-11-30", "Phase2_0", null)]
+        [NUnit.Framework.TestCaseAttribute("2021-11-30", "2021-11-30", "Phase2_0", null)]
         [NUnit.Framework.TestCaseAttribute("2021-04-01", "2021-12-01", "NotSet", null)]
         [NUnit.Framework.TestCaseAttribute("2021-12-01", "2021-06-01", "NotSet", null)]
         public virtual void IncentiveApplicationIsSubmitted(string startDate, string submissionDate, string phase, string[] exampleTags)
