7046bd9d706eca34fd3f8dbe26ee7a5bf7451f91
Kevin <kevin@stephek.co.uk>
2021-05-07 10:58:28 +0100

    EI-1074-update-for-new-requirement

--- src/SFA.DAS.EmployerIncentives.Database/Scripts/Update.SetPhaseForApplications.sql
+++ src/SFA.DAS.EmployerIncentives.Database/Scripts/Update.SetPhaseForApplications.sql
@@ -15,69 +15,20 @@ SET Phase = 'NotSet'
 WHERE Phase IS NULL
 
 --------------------------------------------------------
------------------- Phase1_0 ----------------------------
+------------------ Phase1 ----------------------------
 --------------------------------------------------------
 UPDATE iaa
-SET iaa.Phase = 'Phase1_0'
+SET iaa.Phase = 'Phase1'
 FROM [dbo].[IncentiveApplicationApprenticeship] iaa INNER JOIN [dbo].[IncentiveApplication] ia
 	ON iaa.IncentiveApplicationId = ia.Id
 WHERE iaa.Phase = 'NotSet'
 AND ia.DateSubmitted < '1 June 2021'
-AND iaa.PlannedStartDate >= '1 August 2020'
-AND iaa.PlannedStartDate < '1 February 2021'
 
 UPDATE ai
-SET ai.Phase = 'Phase1_0'
-FROM  [incentives].[ApprenticeshipIncentive] ai LEFT OUTER JOIN [incentives].[Learner] l
-	ON ai.Id = l.ApprenticeshipIncentiveId
+SET ai.Phase = 'Phase1'
+FROM  [incentives].[ApprenticeshipIncentive] ai
 WHERE
 	ai.Phase = 'NotSet'
-AND l.StartDate IS NULL
 AND ai.SubmittedDate < '1 June 2021'
-AND ai.StartDate  >= '1 August 2020'
-AND ai.StartDate < '1 February 2021'
 
-UPDATE ai
-SET ai.Phase = 'Phase1_0'
-FROM  [incentives].[ApprenticeshipIncentive] ai LEFT OUTER JOIN [incentives].[Learner] l
-	ON ai.Id = l.ApprenticeshipIncentiveId
-WHERE
-	ai.Phase = 'NotSet'
-AND l.StartDate IS NOT NULL
-AND ai.SubmittedDate < '1 June 2021'
-AND l.StartDate   >= '1 August 2020'
-AND l.StartDate < '1 February 2021'
 
---------------------------------------------------------
------------------- Phase1_1 ----------------------------
---------------------------------------------------------
-UPDATE iaa
-SET iaa.Phase = 'Phase1_1'
-FROM [dbo].[IncentiveApplicationApprenticeship] iaa INNER JOIN [dbo].[IncentiveApplication] ia
-	ON iaa.IncentiveApplicationId = ia.Id
-WHERE iaa.Phase = 'NotSet'
-AND ia.DateSubmitted < '1 June 2021'
-AND iaa.PlannedStartDate >= '1 February 2021'
-AND iaa.PlannedStartDate < '1 April 2021'
-
-UPDATE ai
-SET ai.Phase = 'Phase1_1'
-FROM  [incentives].[ApprenticeshipIncentive] ai LEFT OUTER JOIN [incentives].[Learner] l
-	ON ai.Id = l.ApprenticeshipIncentiveId
-WHERE
-	ai.Phase = 'NotSet'
-AND l.StartDate IS NULL
-AND ai.SubmittedDate < '1 June 2021'
-AND ai.StartDate  >= '1 February 2021'
-AND ai.StartDate < '1 April 2021'
-
-UPDATE ai
-SET ai.Phase = 'Phase1_1'
-FROM  [incentives].[ApprenticeshipIncentive] ai LEFT OUTER JOIN [incentives].[Learner] l
-	ON ai.Id = l.ApprenticeshipIncentiveId
-WHERE
-	ai.Phase = 'NotSet'
-AND l.StartDate IS NOT NULL
-AND ai.SubmittedDate < '1 June 2021'
-AND l.StartDate   >= '1 February 2021'
-AND l.StartDate < '1 June 2021'
