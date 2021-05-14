ba01f01688abe28aef166713811b8fa8ce4dca42
ruslanss <ruslan.schelkunov@digital.education.gov.uk>
2021-05-14 14:34:14 +0100

    EI-1143 Remove NewAgreementRequired property from IncentiveApplication 🚮 (#311)
    Co-authored-by: Michael Young <mick.young1981@googlemail.com>

--- src/SFA.DAS.EmployerIncentives.Queries/NewApprenticeIncentive/GetApplication/GetApplicationQueryHandler.cs
+++ src/SFA.DAS.EmployerIncentives.Queries/NewApprenticeIncentive/GetApplication/GetApplicationQueryHandler.cs
@@ -1,32 +1,23 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using SFA.DAS.EmployerIncentives.Abstractions.DTOs;
 using SFA.DAS.EmployerIncentives.Abstractions.DTOs.Queries;
 using SFA.DAS.EmployerIncentives.Abstractions.Queries;
 using SFA.DAS.EmployerIncentives.Data;
-using SFA.DAS.EmployerIncentives.Domain.ValueObjects;
 
 namespace SFA.DAS.EmployerIncentives.Queries.NewApprenticeIncentive.GetApplication
 {
     public class GetApplicationQueryHandler : IQueryHandler<GetApplicationRequest, GetApplicationResponse>
     {
         private readonly IQueryRepository<IncentiveApplicationDto> _applicationQueryRepository;
-        private readonly IQueryRepository<LegalEntityDto> _legalEntityQueryRepository;
 
-        public GetApplicationQueryHandler(
-            IQueryRepository<IncentiveApplicationDto> applicationQueryRepository, 
-            IQueryRepository<LegalEntityDto> legalEntityQueryRepository)
+        public GetApplicationQueryHandler(IQueryRepository<IncentiveApplicationDto> applicationQueryRepository)
         {
             _applicationQueryRepository = applicationQueryRepository;
-            _legalEntityQueryRepository = legalEntityQueryRepository;
         }
 
         public async Task<GetApplicationResponse> Handle(GetApplicationRequest query, CancellationToken cancellationToken = default)
         {
             var application = await _applicationQueryRepository.Get(app => app.Id == query.ApplicationId && app.AccountId == query.AccountId);
-
-            var legalEntity = await _legalEntityQueryRepository.Get(x => x.AccountLegalEntityId == application.AccountLegalEntityId);
-            application.NewAgreementRequired = Incentive.IsNewAgreementRequired(application, legalEntity);
 
             var response = new GetApplicationResponse(application);
 
--- src/tests/SFA.DAS.EmployerIncentives.Queries.UnitTests/NewApprenticeIncentive/Handlers/WhenHandlingGetApplicationQuery.cs
+++ src/tests/SFA.DAS.EmployerIncentives.Queries.UnitTests/NewApprenticeIncentive/Handlers/WhenHandlingGetApplicationQuery.cs
@@ -5,9 +5,7 @@ using NUnit.Framework;
 using SFA.DAS.EmployerIncentives.Abstractions.DTOs;
 using SFA.DAS.EmployerIncentives.Abstractions.DTOs.Queries;
 using SFA.DAS.EmployerIncentives.Data;
-using SFA.DAS.EmployerIncentives.Domain.Interfaces;
 using SFA.DAS.EmployerIncentives.Queries.NewApprenticeIncentive.GetApplication;
-using SFA.DAS.EmployerIncentives.UnitTests.Shared.Builders;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -20,16 +18,14 @@ namespace SFA.DAS.EmployerIncentives.Queries.UnitTests.NewApprenticeIncentive.Ha
         private GetApplicationQueryHandler _sut;
         private Mock<IQueryRepository<IncentiveApplicationDto>> _applicationRepository;
         private Fixture _fixture;
-        private Mock<IQueryRepository<LegalEntityDto>> _legalEntityRepository;
 
         [SetUp]
         public void Arrange()
         {
             _fixture = new Fixture();
             _applicationRepository = new Mock<IQueryRepository<IncentiveApplicationDto>>();
-            _legalEntityRepository = new Mock<IQueryRepository<LegalEntityDto>>();
 
-            _sut = new GetApplicationQueryHandler(_applicationRepository.Object, _legalEntityRepository.Object);
+            _sut = new GetApplicationQueryHandler(_applicationRepository.Object);
         }
 
         [Test]
@@ -40,54 +36,14 @@ namespace SFA.DAS.EmployerIncentives.Queries.UnitTests.NewApprenticeIncentive.Ha
             var apprenticeship = _fixture.Build<IncentiveApplicationApprenticeshipDto>().With(x => x.Phase, Enums.Phase.Phase1).With(x => x.PlannedStartDate, new DateTime(2020, 9,1)).Create();
             var data = _fixture.Build<IncentiveApplicationDto>().With(x => x.Apprenticeships, new List<IncentiveApplicationApprenticeshipDto> { apprenticeship }).Create();
             var expected = new GetApplicationResponse(data);
-            var legalEntity = _fixture.Build<LegalEntityDto>().With(x => x.AccountLegalEntityId, data.AccountLegalEntityId).Create();
 
             _applicationRepository.Setup(x => x.Get(dto => dto.Id == query.ApplicationId && dto.AccountId == query.AccountId)).ReturnsAsync(data);
-            _legalEntityRepository.Setup(x => x.Get(y => y.AccountLegalEntityId == data.AccountLegalEntityId)).ReturnsAsync(legalEntity);
 
             //Act
             var result = await _sut.Handle(query, CancellationToken.None);
 
             //Assert
-            result.Should().BeEquivalentTo(expected, opts => opts.Excluding(x => x.Application.NewAgreementRequired));
-        }
-
-        [Test]
-        public async Task Then_a_new_agreement_is_required_when_employer_has_not_signed_required_agreement_version()
-        {
-            //Arrange
-            var query = _fixture.Create<GetApplicationRequest>();
-            var apprenticeship = _fixture.Build<IncentiveApplicationApprenticeshipDto>().With(x => x.Phase, Enums.Phase.Phase1).With(x => x.PlannedStartDate, new DateTime(2021, 2, 1)).Create();
-            var data = _fixture.Build<IncentiveApplicationDto>().With(x => x.Apprenticeships, new List<IncentiveApplicationApprenticeshipDto> { apprenticeship }).Create();
-            var legalEntity = _fixture.Build<LegalEntityDto>().With(x => x.AccountLegalEntityId, data.AccountLegalEntityId).With(x => x.SignedAgreementVersion, 4).Create();
-
-            _applicationRepository.Setup(x => x.Get(dto => dto.Id == query.ApplicationId && dto.AccountId == query.AccountId)).ReturnsAsync(data);
-            _legalEntityRepository.Setup(x => x.Get(y => y.AccountLegalEntityId == data.AccountLegalEntityId)).ReturnsAsync(legalEntity);
-
-            //Act
-            var result = await _sut.Handle(query, CancellationToken.None);
-
-            //Assert
-            result.Application.NewAgreementRequired.Should().BeTrue();
-        }
-
-        [Test]
-        public async Task Then_a_new_agreement_is_not_required_when_employer_has_signed_required_agreement_version()
-        {
-            //Arrange
-            var query = _fixture.Create<GetApplicationRequest>();
-            var apprenticeship = _fixture.Build<IncentiveApplicationApprenticeshipDto>().With(x => x.Phase, Enums.Phase.Phase1).With(x => x.PlannedStartDate, new DateTime(2020, 9, 1)).Create();
-            var data = _fixture.Build<IncentiveApplicationDto>().With(x => x.Apprenticeships, new List<IncentiveApplicationApprenticeshipDto> { apprenticeship }).Create();
-            var legalEntity = _fixture.Build<LegalEntityDto>().With(x => x.AccountLegalEntityId, data.AccountLegalEntityId).With(x => x.SignedAgreementVersion, 5).Create();
-
-            _applicationRepository.Setup(x => x.Get(dto => dto.Id == query.ApplicationId && dto.AccountId == query.AccountId)).ReturnsAsync(data);
-            _legalEntityRepository.Setup(x => x.Get(y => y.AccountLegalEntityId == data.AccountLegalEntityId)).ReturnsAsync(legalEntity);
-
-            //Act
-            var result = await _sut.Handle(query, CancellationToken.None);
-
-            //Assert
-            result.Application.NewAgreementRequired.Should().BeFalse();
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
