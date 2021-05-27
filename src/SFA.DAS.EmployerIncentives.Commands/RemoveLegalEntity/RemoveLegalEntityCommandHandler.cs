1237d8303245942c13dcf79b33f4c44964dd8ce8
Dave Read <david.read@digital.education.gov.uk>
2021-05-27 14:49:50 +0100

    EI-1104 withdraw apprentices for deleted legal entities

--- src/SFA.DAS.EmployerIncentives.Commands/RemoveLegalEntity/RemoveLegalEntityCommandHandler.cs
+++ src/SFA.DAS.EmployerIncentives.Commands/RemoveLegalEntity/RemoveLegalEntityCommandHandler.cs
@@ -1,30 +1,33 @@
-﻿using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+﻿using System.Collections.Generic;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
 using SFA.DAS.EmployerIncentives.Commands.Persistence;
 using System.Threading;
 using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Commands.Types.ApprenticeshipIncentive;
 
 namespace SFA.DAS.EmployerIncentives.Commands.RemoveLegalEntity
 {
     public class RemoveLegalEntityCommandHandler : ICommandHandler<RemoveLegalEntityCommand>
     {
-        private readonly IAccountDomainRepository _domainRepository;
+        private readonly IAccountDomainRepository _accountDomainRepository;
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IIncentiveApplicationDomainRepository _incentiveApplicationDomainRepository;
 
-        public RemoveLegalEntityCommandHandler(IAccountDomainRepository domainRepository, CancellationToken cancellationToken = default)
+        public RemoveLegalEntityCommandHandler(IAccountDomainRepository accountDomainRepository, 
+            IIncentiveApplicationDomainRepository incentiveApplicationDomainRepository,
+            ICommandDispatcher commandDispatcher, 
+            CancellationToken cancellationToken = default)
         {
-            _domainRepository = domainRepository;
+            _accountDomainRepository = accountDomainRepository;
+            _incentiveApplicationDomainRepository = incentiveApplicationDomainRepository;
+            _commandDispatcher = commandDispatcher;
         }
 
         public async Task Handle(RemoveLegalEntityCommand command, CancellationToken cancellationToken = default)
         {
-            var account = await _domainRepository.Find(command.AccountId);
+            var account = await _accountDomainRepository.Find(command.AccountId);
 
-            if (account == null)
-            {
-                // already deleted
-                return;
-            }
-
-            var legalEntity = account.GetLegalEntity(command.AccountLegalEntityId);
+            var legalEntity = account?.GetLegalEntity(command.AccountLegalEntityId);
             if (legalEntity == null)
             {
                 // already deleted
@@ -32,8 +35,23 @@ namespace SFA.DAS.EmployerIncentives.Commands.RemoveLegalEntity
             }
 
             account.RemoveLegalEntity(legalEntity);
+            
+            var applications = await _incentiveApplicationDomainRepository.FindByAccountLegalEntity(command.AccountLegalEntityId);
 
-            await _domainRepository.Save(account);
+            var withdrawTasks = new List<Task>();
+            foreach(var application in applications)
+            {
+                foreach(var apprenticeship in application.Apprenticeships)
+                {
+                    var withdrawCommand = new WithdrawCommand(application.AccountId, apprenticeship.Id);
+
+                    withdrawTasks.Add(_commandDispatcher.Send(withdrawCommand));
+                }
+            }
+
+            await Task.WhenAll(withdrawTasks);
+            
+            await _accountDomainRepository.Save(account);
         }
     }
 }
--- src/tests/SFA.DAS.EmployerIncentives.Commands.UnitTests/RemoveLegalEntity/Handlers/WhenHandlingRemoveLegalEntityCommand.cs
+++ src/tests/SFA.DAS.EmployerIncentives.Commands.UnitTests/RemoveLegalEntity/Handlers/WhenHandlingRemoveLegalEntityCommand.cs
@@ -1,19 +1,28 @@
-﻿using AutoFixture;
+﻿using System;
+using System.Collections.Generic;
+using AutoFixture;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerIncentives.Commands.Persistence;
 using SFA.DAS.EmployerIncentives.Commands.RemoveLegalEntity;
-using SFA.DAS.EmployerIncentives.Domain.Accounts;
 using SFA.DAS.EmployerIncentives.Domain.Accounts.Models;
 using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+using SFA.DAS.EmployerIncentives.Commands.Types.ApprenticeshipIncentive;
+using Account = SFA.DAS.EmployerIncentives.Domain.Accounts.Account;
+using SFA.DAS.EmployerIncentives.Domain.IncentiveApplications;
 
 namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Handlers
 {
+    [TestFixture]
     public class WhenHandlingRemoveLegalEntityCommand
     {
         private RemoveLegalEntityCommandHandler _sut;
-        private Mock<IAccountDomainRepository> _mockDomainRespository;
+        private Mock<IAccountDomainRepository> _accountDomainRepository;
+        private Mock<IIncentiveApplicationDomainRepository> _applicationDomainRepository;
+        private Mock<ICommandDispatcher> _commandDispatcher;
         
         private Fixture _fixture;
 
@@ -22,9 +31,11 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
         {
             _fixture = new Fixture();
 
-            _mockDomainRespository = new Mock<IAccountDomainRepository>();
+            _accountDomainRepository = new Mock<IAccountDomainRepository>();
+            _applicationDomainRepository = new Mock<IIncentiveApplicationDomainRepository>();
+            _commandDispatcher = new Mock<ICommandDispatcher>();
             
-            _sut = new RemoveLegalEntityCommandHandler(_mockDomainRespository.Object);
+            _sut = new RemoveLegalEntityCommandHandler(_accountDomainRepository.Object, _applicationDomainRepository.Object, _commandDispatcher.Object);
         }
 
         [Test]
@@ -36,7 +47,7 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
             legalEntityModel.Id = command.AccountId;
             legalEntityModel.AccountLegalEntityId = command.AccountLegalEntityId;
 
-            _mockDomainRespository
+            _accountDomainRepository
                 .Setup(m => m.Find(command.AccountId))
                 .ReturnsAsync(Account.Create(
                     new AccountModel
@@ -53,7 +64,7 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
             await _sut.Handle(command);
 
             //Assert
-            _mockDomainRespository.Verify(m => m.Save(It.Is<Account>(i => i.Id == command.AccountId && i.LegalEntities.Count == 2)), Times.Once);
+            _accountDomainRepository.Verify(m => m.Save(It.Is<Account>(i => i.Id == command.AccountId && i.LegalEntities.Count == 2)), Times.Once);
         }
 
         [Test]
@@ -61,7 +72,7 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
         {
             //Arrange
             var command = _fixture.Create<RemoveLegalEntityCommand>();
-            _mockDomainRespository
+            _accountDomainRepository
                 .Setup(m => m.Find(command.AccountId))
                 .ReturnsAsync(null as Account);
 
@@ -69,7 +80,7 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
             await _sut.Handle(command);
 
             //Assert
-            _mockDomainRespository.Verify(m => m.Save(It.Is<Account>(i => i.Id == command.AccountId)), Times.Never);
+            _accountDomainRepository.Verify(m => m.Save(It.Is<Account>(i => i.Id == command.AccountId)), Times.Never);
         }
 
         [Test]
@@ -77,7 +88,7 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
         {
             //Arrange
             var command = _fixture.Create<RemoveLegalEntityCommand>();
-            _mockDomainRespository
+            _accountDomainRepository
                 .Setup(m => m.Find(command.AccountId))
                 .ReturnsAsync(Account.Create(new AccountModel { Id = 1, LegalEntityModels = new Collection<LegalEntityModel>() { _fixture.Create<LegalEntityModel>() } }));
 
@@ -85,7 +96,56 @@ namespace SFA.DAS.EmployerIncentives.Application.UnitTests.RemoveLegalEntity.Han
             await _sut.Handle(command);
 
             //Assert
-            _mockDomainRespository.Verify(m => m.Save(It.Is<Account>(i => i.Id == command.AccountId)), Times.Never);
+            _accountDomainRepository.Verify(m => m.Save(It.Is<Account>(i => i.Id == command.AccountId)), Times.Never);
+        }
+
+        [Test]
+        public async Task Then_any_applications_for_the_account_legal_entity_are_withdrawn()
+        {
+            //Arrange
+            var command = _fixture.Create<RemoveLegalEntityCommand>();
+            var legalEntityModel = _fixture.Create<LegalEntityModel>();
+            legalEntityModel.Id = command.AccountId;
+            legalEntityModel.AccountLegalEntityId = command.AccountLegalEntityId;
+
+            _accountDomainRepository
+                .Setup(m => m.Find(command.AccountId))
+                .ReturnsAsync(Account.Create(
+                    new AccountModel
+                    {
+                        Id = command.AccountId,
+                        LegalEntityModels = new Collection<LegalEntityModel>() {
+                            _fixture.Create<LegalEntityModel>(),
+                            legalEntityModel
+                        }
+                    }));
+
+            var applications = new List<IncentiveApplication>
+            {
+                IncentiveApplication.New(Guid.NewGuid(), command.AccountId, command.AccountLegalEntityId),
+                IncentiveApplication.New(Guid.NewGuid(), command.AccountId, command.AccountLegalEntityId),
+                IncentiveApplication.New(Guid.NewGuid(), command.AccountId, command.AccountLegalEntityId)
+            };
+            foreach(var application in applications)
+            {
+                application.SetApprenticeships(_fixture.CreateMany<Apprenticeship>(2));
+            }
+
+            _applicationDomainRepository.Setup(x => x.FindByAccountLegalEntity(command.AccountLegalEntityId))
+                .ReturnsAsync(applications);
+
+            //Act
+            await _sut.Handle(command);
+
+            //Assert
+            _commandDispatcher.Verify(x => x.Send(It.Is<WithdrawCommand>(y => y.AccountId == command.AccountId), It.IsAny<CancellationToken>()), Times.Exactly(6));
+            foreach(var application in applications)
+            {
+                foreach(var apprenticeship in application.Apprenticeships)
+                {
+                    _commandDispatcher.Verify(x => x.Send(It.Is<WithdrawCommand>(y => y.IncentiveApplicationApprenticeshipId == apprenticeship.Id), It.IsAny<CancellationToken>()), Times.Once);
+                }
+            }
         }
     }
 }
