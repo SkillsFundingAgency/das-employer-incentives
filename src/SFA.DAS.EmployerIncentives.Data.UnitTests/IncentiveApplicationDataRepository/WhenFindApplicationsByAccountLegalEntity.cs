8f69e890444d0d1dc24b0658c536aff174891121
Dave Read <david.read@digital.education.gov.uk>
2021-05-28 13:10:41 +0100

    EI-1104 add repository unit tests

---
+++ src/SFA.DAS.EmployerIncentives.Data.UnitTests/IncentiveApplicationDataRepository/WhenFindApplicationsByAccountLegalEntity.cs
@@ -0,0 +1,73 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Data.IncentiveApplication;
+using SFA.DAS.EmployerIncentives.Data.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Data.UnitTests.IncentiveApplicationDataRepository
+{
+    [TestFixture]
+    public class WhenFindApplicationsByAccountLegalEntity
+    {
+        private IIncentiveApplicationDataRepository _sut;
+        private readonly Fixture _fixture = new Fixture();
+        private EmployerIncentivesDbContext _dbContext;
+
+        [SetUp]
+        public void Arrange()
+        {
+            var options = new DbContextOptionsBuilder<EmployerIncentivesDbContext>()
+                .UseInMemoryDatabase("EmployerIncentivesDbContext" + Guid.NewGuid()).Options;
+            _dbContext = new EmployerIncentivesDbContext(options);
+
+            _sut = new IncentiveApplication.IncentiveApplicationDataRepository(
+                new Lazy<EmployerIncentivesDbContext>(_dbContext));
+        }
+
+        [TearDown]
+        public void CleanUp() => _dbContext.Dispose();
+
+        [Test]
+        public async Task Then_the_incentive_applications_for_the_account_legal_entity_are_returned()
+        {
+            // Arrange
+            var apps = _fixture.CreateMany<Models.IncentiveApplication>(3).ToList();
+            apps[0].AccountLegalEntityId = 123;
+            apps[1].AccountLegalEntityId = 456;
+            apps[2].AccountLegalEntityId = 123;
+
+            await _dbContext.AddRangeAsync(apps);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _sut.FindApplicationsByAccountLegalEntity(123);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Count().Should().Be(2);
+        }
+
+        [Test]
+        public async Task Then_no_applications_are_returned_if_none_match_the_account_legal_entity()
+        {
+            // Arrange
+            var apps = _fixture.CreateMany<Models.IncentiveApplication>(3).ToList();
+            apps[0].AccountLegalEntityId = 900;
+            apps[1].AccountLegalEntityId = 800;
+            apps[2].AccountLegalEntityId = 700;
+
+            await _dbContext.AddRangeAsync(apps);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _sut.FindApplicationsByAccountLegalEntity(123);
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+    }
+}
