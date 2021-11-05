a4b15ed1d96e71b8f75412ad32ecc14c0b7e630d
David-B-Read <david.read@digital.education.gov.uk>
2021-11-05 14:28:21 +0000

    EI-1524 set bank details required for account legal entity

--- src/SFA.DAS.EmployerIncentives.Data/Account/AccountQueryRepository.cs
+++ src/SFA.DAS.EmployerIncentives.Data/Account/AccountQueryRepository.cs
@@ -43,7 +43,8 @@ namespace SFA.DAS.EmployerIncentives.Data.Account
                 VrfVendorId = x.VrfVendorId,
                 VrfCaseStatus = x.VrfCaseStatus,
                 HashedLegalEntityId = x.HashedLegalEntityId,
-                IsAgreementSigned = x.SignedAgreementVersion.HasValue && x.SignedAgreementVersion >= Phase2Incentive.MinimumAgreementVersion()
+                IsAgreementSigned = x.SignedAgreementVersion.HasValue && x.SignedAgreementVersion >= Phase2Incentive.MinimumAgreementVersion(),
+                BankDetailsRequired = String.IsNullOrWhiteSpace(x.VrfCaseStatus)
             };
         }
 
