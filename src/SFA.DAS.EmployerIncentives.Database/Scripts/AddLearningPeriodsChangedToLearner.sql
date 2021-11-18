88d2c7906698ba540d6f9808c1853476ded72397
Michael Young <mick.young1981@googlemail.com>
2021-11-18 09:29:53 +0000

    Alter table in pre-release script.

---
+++ src/SFA.DAS.EmployerIncentives.Database/Scripts/AddLearningPeriodsChangedToLearner.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS(SELECT *
+          FROM   INFORMATION_SCHEMA.COLUMNS
+          WHERE TABLE_SCHEMA = 'incentives'
+		  AND TABLE_NAME = 'Learner'
+          AND COLUMN_NAME = 'LearningPeriodsChanged') 
+BEGIN
+	ALTER TABLE incentives.Learner
+        ADD [LearningPeriodsChanged] BIT NOT NULL DEFAULT(0)
+END
