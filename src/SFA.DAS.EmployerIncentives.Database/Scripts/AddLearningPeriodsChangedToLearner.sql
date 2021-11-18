b7b0bcc4185c8cbac4e80d4edf96834275c6e3b4
Michael Young <mick.young1981@googlemail.com>
2021-11-18 10:25:24 +0000

    Change script so it works when the db doesn't exist!

--- src/SFA.DAS.EmployerIncentives.Database/Scripts/AddLearningPeriodsChangedToLearner.sql
+++ src/SFA.DAS.EmployerIncentives.Database/Scripts/AddLearningPeriodsChangedToLearner.sql
@@ -1,9 +1,12 @@
-﻿IF NOT EXISTS(SELECT *
-          FROM   INFORMATION_SCHEMA.COLUMNS
-          WHERE TABLE_SCHEMA = 'incentives'
-		  AND TABLE_NAME = 'Learner'
-          AND COLUMN_NAME = 'LearningPeriodsChanged') 
+﻿IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'incentives' AND TABLE_NAME = 'Learner')
 BEGIN
-	ALTER TABLE incentives.Learner
-        ADD [LearningPeriodsChanged] BIT NOT NULL DEFAULT(0)
+    IF NOT EXISTS(SELECT *
+              FROM   INFORMATION_SCHEMA.COLUMNS
+              WHERE TABLE_SCHEMA = 'incentives'
+		      AND TABLE_NAME = 'Learner'
+              AND COLUMN_NAME = 'LearningPeriodsChanged') 
+    BEGIN
+	    ALTER TABLE incentives.Learner
+            ADD [LearningPeriodsChanged] BIT NOT NULL DEFAULT(0)
+    END
 END
