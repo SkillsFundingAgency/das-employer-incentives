a4357af522c88c1785c7833786e2c02ec9c1324b
Michael Young <mick.young1981@googlemail.com>
2021-11-24 15:22:49 +0000

    Added database table.

---
+++ src/SFA.DAS.EmployerIncentives.Database/Tables/EmploymentCheck.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [incentives].[EmploymentCheck]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[ApprenticeshipIncentiveId] UNIQUEIDENTIFIER NOT NULL,
+	[CheckType] NVARCHAR(50) NOT NULL,
+	[MinimumDate] DATETIME2 NOT NULL,
+	[MaximumDate] DATETIME2 NOT NULL,
+	[CorrelationId] UNIQUEIDENTIFIER NOT NULL,
+	[Result] BIT NULL,
+	[CreatedDateTime] DATETIME2 NOT NULL,
+	[ResultDateTime] DATETIME2 NULL,
+	CONSTRAINT FK_EC_ApprenticeshipIncentive FOREIGN KEY (ApprenticeshipIncentiveId) REFERENCES [incentives].[ApprenticeshipIncentive](Id), 
+)
+GO
+CREATE CLUSTERED INDEX IX_EmploymentCheck ON [incentives].[EmploymentCheck] (ApprenticeshipIncentiveId)
