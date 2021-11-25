1b29a0b38e7eb4d121f5a39f9f3fa37c57110b5e
Michael Young <mick.young1981@googlemail.com>
2021-11-25 08:53:17 +0000

    Added data model.

---
+++ src/SFA.DAS.EmployerIncentives.Data/ApprenticeshipIncentives/Models/EmploymentCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using SFA.DAS.EmployerIncentives.Enums;
+
+namespace SFA.DAS.EmployerIncentives.Data.ApprenticeshipIncentives.Models
+{
+    [Dapper.Contrib.Extensions.Table("incentives.EmploymentCheck")]
+    [Table("EmploymentCheck", Schema = "incentives")]
+    public partial class EmploymentCheck
+    {
+        [Dapper.Contrib.Extensions.ExplicitKey]
+        public Guid Id { get; set; }
+        public Guid ApprenticeshipIncentiveId { get; set; }
+        public EmploymentCheckType CheckType { get; set; }
+        public DateTime MinimumDate { get; set; }
+        public DateTime MaximumDate { get; set; }
+        public Guid CorrelationId { get; set; }
+        public bool? Result { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime? ResultDateTime { get; set; }
+    }
+}
