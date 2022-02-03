4d9371450c7ee42c5ec1b43f3f799fcea8144616
Kevin <kevin@stephek.co.uk>
2022-02-03 15:01:54 +0000

    EI-1615-test

--- src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/TestWebApi.cs
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/TestWebApi.cs
@@ -94,7 +94,7 @@ namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests
 
                 Commands.ServiceCollectionExtensions.AddCommandHandlers(s, AddDecorators);
 
-                s.AddTransient<IDistributedLockProvider, TestDistributedLockProvider>();
+                s.AddTransient<IDistributedLockProvider, NullLockProvider>();
                 s.Decorate<IEventPublisher>((handler, sp) => new TestEventPublisher(handler, _eventMessageHook));
                 s.Decorate<ICommandPublisher>((handler, sp) => new TestCommandPublisher(handler, _commandMessageHook));                
                 s.Decorate<IScheduledCommandPublisher>((handler, sp) => new TestScheduledCommandPublisher(handler, _commandMessageHook));
