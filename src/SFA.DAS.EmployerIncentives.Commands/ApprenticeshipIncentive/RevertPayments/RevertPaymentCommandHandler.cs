1e1b9f3420df01c0ee5b5f9f18fb0247611f2d06
David-B-Read <david.read@digital.education.gov.uk>
2022-06-15 10:29:11 +0100

    EI-1851 create API endpoint

---
+++ src/SFA.DAS.EmployerIncentives.Commands/ApprenticeshipIncentive/RevertPayments/RevertPaymentCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+using SFA.DAS.EmployerIncentives.Commands.Persistence;
+using SFA.DAS.EmployerIncentives.Domain.ValueObjects;
+
+namespace SFA.DAS.EmployerIncentives.Commands.ApprenticeshipIncentive.RevertPayments
+{
+    public class RevertPaymentCommandHandler : ICommandHandler<RevertPaymentCommand>
+    {
+        private readonly IApprenticeshipIncentiveDomainRepository _incentiveDomainRepository;
+
+        public RevertPaymentCommandHandler(IApprenticeshipIncentiveDomainRepository incentiveDomainRepository)
+        {
+            _incentiveDomainRepository = incentiveDomainRepository;
+        }
+
+        public async Task Handle(RevertPaymentCommand command, CancellationToken cancellationToken = default)
+        {
+            var incentive = await _incentiveDomainRepository.FindByPaymentId(command.PaymentId);
+
+            incentive.RevertPayment(command.PaymentId, new ServiceRequest(command.ServiceRequestId, command.DecisionReferenceNumber, command.DateServiceRequestTaskCreated.Value));
+
+            await _incentiveDomainRepository.Save(incentive);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Commands/ApprenticeshipIncentive/RevertPayments/RevertPaymentCommandValidator.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Abstractions.Commands;
+
+namespace SFA.DAS.EmployerIncentives.Commands.ApprenticeshipIncentive.RevertPayments
+{
+    public class RevertPaymentCommandValidator : IValidator<RevertPaymentCommand>
+    {
+        public Task<ValidationResult> Validate(RevertPaymentCommand item)
+        {
+            var result = new ValidationResult();
+
+            if (item.PaymentId == default)
+            {
+                result.AddError("PaymentId", "Is not set");
+            }
+
+            if (item.ServiceRequestId == default)
+            {
+                result.AddError("ServiceRequestId", "Is not set");
+            }
+
+            if (item.DecisionReferenceNumber == default)
+            {
+                result.AddError("DecisionReferenceNumber", "Is not set");
+            }
+            
+            if (item.DateServiceRequestTaskCreated == default)
+            {
+                result.AddError("DateServiceRequestTaskCreated", "Is not set");
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
