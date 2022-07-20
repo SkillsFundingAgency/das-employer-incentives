a594c695d02a04b97e845cd3550374cc3d170f12
David-B-Read <david.read@digital.education.gov.uk>
2022-07-20 13:31:19 +0100

    EI-1910 add unit tests

---
+++ src/SFA.DAS.EmployerIncentives.Data.UnitTests/ApprenticeshipIncentiveArchive/WhenGetArchivePendingPaymentModelCalled.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Data.Models;
+
+namespace SFA.DAS.EmployerIncentives.Data.UnitTests.ApprenticeshipIncentiveArchive
+{
+    [TestFixture]
+    public class WhenGetArchivePendingPaymentModelCalled
+    {
+        private ApprenticeshipIncentives.ApprenticeshipIncentiveArchiveRepository _sut;
+        private Fixture _fixture;
+        private EmployerIncentivesDbContext _dbContext;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new Fixture();
+
+            var options = new DbContextOptionsBuilder<EmployerIncentivesDbContext>()
+                .UseInMemoryDatabase("EmployerIncentivesDbContext" + Guid.NewGuid()).Options;
+            _dbContext = new EmployerIncentivesDbContext(options);
+
+            _sut = new ApprenticeshipIncentives.ApprenticeshipIncentiveArchiveRepository(new Lazy<EmployerIncentivesDbContext>(_dbContext));
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _dbContext.Dispose();
+        }
+
+        [Test]
+        public async Task Then_the_archived_pending_payment_is_retrieved_from_the_data_store()
+        {
+            // Arrange
+            var pendingPaymentId = Guid.NewGuid();
+
+            var archivedPendingPayment = _fixture.Build<ApprenticeshipIncentives.Models.Archive.PendingPayment>()
+                .With(x => x.PendingPaymentId, pendingPaymentId)
+                .Create();
+
+            await _dbContext.AddAsync(archivedPendingPayment);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var pendingPayment = await _sut.Get(pendingPaymentId);
+
+            // Assert
+            pendingPayment.Should().NotBeNull();
+            pendingPayment.Id.Should().Be(pendingPaymentId);
+            pendingPayment.ApprenticeshipIncentiveId.Should().Be(archivedPendingPayment.ApprenticeshipIncentiveId);
+            pendingPayment.Amount.Should().Be(archivedPendingPayment.Amount);
+            pendingPayment.EarningType.Should().Be(archivedPendingPayment.EarningType);
+        }
+    }
+}
