e89b26c790e7b0e202172607a8c5196e57b90d5c
David-B-Read <david.read@digital.education.gov.uk>
2022-08-16 12:10:07 +0100

    EI-1931 remove cut off date and restrict results to current pay run

--- src/SFA.DAS.EmployerIncentives.Database/Scripts/Generate.EmploymentCheckFailureEmailsReport.sql
+++ src/SFA.DAS.EmployerIncentives.Database/Scripts/Generate.EmploymentCheckFailureEmailsReport.sql
@@ -1,14 +1,37 @@
 DECLARE @PeriodNumber TINYINT
 DECLARE @PaymentYear SMALLINT
-DECLARE @employmentCheckimportDate DATETIME
 
 -- Update period & payment year here
-SET @PeriodNumber = 8
+SET @PeriodNumber = 12
 SET @PaymentYear = 2122
 -- Update period & payment year here
--- Update the date the employment check results were imported here
-SET @employmentCheckimportDate = '2022-03-28'
--- Update the date the employment check results were imported here
+
+DECLARE @EmployedAtStartOfApprenticeshipFailures TABLE 
+(
+	PendingPaymentId UNIQUEIDENTIFIER
+)
+
+DECLARE @EmployedBeforeSchemeStartedFailures TABLE 
+(
+	PendingPaymentId UNIQUEIDENTIFIER
+)
+
+-- Pending payments that have faiiled EC validation in previous runs
+INSERT INTO @EmployedAtStartOfApprenticeshipFailures
+SELECT DISTINCT PendingPaymentId 
+FROM incentives.PendingPaymentValidationResult
+WHERE Step = 'EmployedAtStartOfApprenticeship' 
+AND Result = 0 
+AND PeriodNumber <> @PeriodNumber 
+OR PaymentYear <> @PaymentYear
+
+INSERT INTO @EmployedBeforeSchemeStartedFailures
+SELECT DISTINCT PendingPaymentId 
+FROM incentives.PendingPaymentValidationResult
+WHERE Step = 'EmployedBeforeSchemeStarted' 
+AND Result = 0 
+AND PeriodNumber <> @PeriodNumber 
+OR PaymentYear <> @PaymentYear
 
 -- Failed 1 or Both
 SELECT DISTINCT x.SubmittedByEmail, SUBSTRING(ia.SubmittedByName, 0, CHARINDEX(' ', ia.SubmittedByName)) as [First Name], TRIM(SUBSTRING(ia.SubmittedByName, CHARINDEX(' ', ia.SubmittedByName), LEN(ia.SubmittedByName)-CHARINDEX(' ', ia.SubmittedByName)+1)) AS [Last Name], '1OrBoth' AS 'CheckType' FROM
@@ -17,17 +40,37 @@ SELECT DISTINCT x.SubmittedByEmail, SUBSTRING(ia.SubmittedByName, 0, CHARINDEX('
 	ai.SubmittedByEmail,
 	ai.IncentiveApplicationApprenticeshipId,
 	ai.PausePayments,
-	(SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC1 WHERE ppvrEC1.PendingPaymentId = pp.Id AND ppvrEC1.Step = 'EmployedAtStartOfApprenticeship' AND ppvrEC1.PeriodNumber = @PeriodNumber AND ppvrEC1.PaymentYear = @PaymentYear) AS EmployedAtStartOfApprenticeship,
-	(SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC2 WHERE ppvrEC2.PendingPaymentId = pp.Id AND ppvrEC2.Step = 'EmployedBeforeSchemeStarted' AND ppvrEC2.PeriodNumber = @PeriodNumber AND ppvrEC2.PaymentYear = @PaymentYear) AS EmployedBeforeSchemeStarted,
-	(SELECT Result FROM incentives.PendingPaymentValidationResult ppvrLearn WHERE ppvrLearn.PendingPaymentId = pp.Id AND ppvrLearn.Step = 'HasLearningRecord' AND ppvrLearn.PeriodNumber = @PeriodNumber AND ppvrLearn.PaymentYear = @PaymentYear) AS HasLearningRecord
+	(
+	SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC1 
+	WHERE ppvrEC1.PendingPaymentId = pp.Id 
+	AND ppvrEC1.Step = 'EmployedAtStartOfApprenticeship' 
+	AND ppvrEC1.PeriodNumber = @PeriodNumber 
+	AND ppvrEC1.PaymentYear = @PaymentYear
+	AND pp.Id NOT IN (SELECT PendingPaymentId FROM @EmployedAtStartOfApprenticeshipFailures)
+	) AS EmployedAtStartOfApprenticeship,
+	(
+	SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC2 
+	WHERE ppvrEC2.PendingPaymentId = pp.Id 
+	AND ppvrEC2.Step = 'EmployedBeforeSchemeStarted' 
+	AND ppvrEC2.PeriodNumber = @PeriodNumber 
+	AND ppvrEC2.PaymentYear = @PaymentYear
+	AND pp.Id NOT IN (SELECT PendingPaymentId FROM @EmployedBeforeSchemeStartedFailures)
+	) AS EmployedBeforeSchemeStarted,
+	(
+	SELECT Result FROM incentives.PendingPaymentValidationResult ppvrLearn 
+	WHERE ppvrLearn.PendingPaymentId = pp.Id 
+	AND ppvrLearn.Step = 'HasLearningRecord' 
+	AND ppvrLearn.PeriodNumber = @PeriodNumber 
+	AND ppvrLearn.PaymentYear = @PaymentYear
+	) AS HasLearningRecord
 FROM incentives.ApprenticeshipIncentive ai
 INNER JOIN incentives.PendingPayment pp ON pp.ApprenticeshipIncentiveId = ai.Id) x
 INNER JOIN Accounts a ON a.AccountLegalEntityId = x.AccountLegalEntityId
 INNER JOIN IncentiveApplicationApprenticeship iaa ON iaa.Id = x.IncentiveApplicationApprenticeshipId
 INNER JOIN IncentiveApplication ia ON ia.Id = iaa.IncentiveApplicationId
 WHERE x.HasLearningRecord = 1 AND x.EmployedBeforeSchemeStarted = 0 AND x.PausePayments = 0
-AND ((SELECT COUNT(1) FROM incentives.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL AND ec.CreatedDateTime >= @employmentCheckimportDate) = 2
-OR (SELECT COUNT(1) FROM archive.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL AND ec.CreatedDateTime >= @employmentCheckimportDate) = 2)
+AND ((SELECT COUNT(1) FROM incentives.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL) = 2
+OR (SELECT COUNT(1) FROM archive.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL) = 2)
 
 -- Failed check 2
 SELECT DISTINCT x.SubmittedByEmail, SUBSTRING(ia.SubmittedByName, 0, CHARINDEX(' ', ia.SubmittedByName)) as [First Name], TRIM(SUBSTRING(ia.SubmittedByName, CHARINDEX(' ', ia.SubmittedByName), LEN(ia.SubmittedByName)-CHARINDEX(' ', ia.SubmittedByName)+1)) AS [Last Name], '2nd' AS 'CheckType' FROM
@@ -36,14 +79,34 @@ SELECT DISTINCT x.SubmittedByEmail, SUBSTRING(ia.SubmittedByName, 0, CHARINDEX('
 	ai.SubmittedByEmail,
 	ai.IncentiveApplicationApprenticeshipId,
 	ai.PausePayments,
-	(SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC1 WHERE ppvrEC1.PendingPaymentId = pp.Id AND ppvrEC1.Step = 'EmployedAtStartOfApprenticeship' AND ppvrEC1.PeriodNumber = @PeriodNumber AND ppvrEC1.PaymentYear = @PaymentYear) AS EmployedAtStartOfApprenticeship,
-	(SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC2 WHERE ppvrEC2.PendingPaymentId = pp.Id AND ppvrEC2.Step = 'EmployedBeforeSchemeStarted' AND ppvrEC2.PeriodNumber = @PeriodNumber AND ppvrEC2.PaymentYear = @PaymentYear) AS EmployedBeforeSchemeStarted,
-	(SELECT Result FROM incentives.PendingPaymentValidationResult ppvrLearn WHERE ppvrLearn.PendingPaymentId = pp.Id AND ppvrLearn.Step = 'HasLearningRecord' AND ppvrLearn.PeriodNumber = @PeriodNumber AND ppvrLearn.PaymentYear = @PaymentYear) AS HasLearningRecord
+	(
+	SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC1 
+	WHERE ppvrEC1.PendingPaymentId = pp.Id 
+	AND ppvrEC1.Step = 'EmployedAtStartOfApprenticeship' 
+	AND ppvrEC1.PeriodNumber = @PeriodNumber 
+	AND ppvrEC1.PaymentYear = @PaymentYear
+	AND pp.Id NOT IN (SELECT PendingPaymentId FROM @EmployedAtStartOfApprenticeshipFailures)
+	) AS EmployedAtStartOfApprenticeship,
+	(
+	SELECT Result FROM incentives.PendingPaymentValidationResult ppvrEC2 
+	WHERE ppvrEC2.PendingPaymentId = pp.Id 
+	AND ppvrEC2.Step = 'EmployedBeforeSchemeStarted' 
+	AND ppvrEC2.PeriodNumber = @PeriodNumber 
+	AND ppvrEC2.PaymentYear = @PaymentYear
+	AND pp.Id NOT IN (SELECT PendingPaymentId FROM @EmployedBeforeSchemeStartedFailures)
+	) AS EmployedBeforeSchemeStarted,
+	(
+	SELECT Result FROM incentives.PendingPaymentValidationResult ppvrLearn 
+	WHERE ppvrLearn.PendingPaymentId = pp.Id 
+	AND ppvrLearn.Step = 'HasLearningRecord' 
+	AND ppvrLearn.PeriodNumber = @PeriodNumber 
+	AND ppvrLearn.PaymentYear = @PaymentYear
+	) AS HasLearningRecord
 FROM incentives.ApprenticeshipIncentive ai
 INNER JOIN incentives.PendingPayment pp ON pp.ApprenticeshipIncentiveId = ai.Id) x
 INNER JOIN Accounts a ON a.AccountLegalEntityId = x.AccountLegalEntityId
 INNER JOIN IncentiveApplicationApprenticeship iaa ON iaa.Id = x.IncentiveApplicationApprenticeshipId
 INNER JOIN IncentiveApplication ia ON ia.Id = iaa.IncentiveApplicationId
 WHERE x.HasLearningRecord = 1 AND x.EmployedAtStartOfApprenticeship = 0 AND x.EmployedBeforeSchemeStarted = 1 AND x.PausePayments = 0
-AND ((SELECT COUNT(1) FROM incentives.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL AND ec.CreatedDateTime >= @employmentCheckimportDate) = 2
-OR (SELECT COUNT(1) FROM archive.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL AND ec.CreatedDateTime >= @employmentCheckimportDate) = 2)
+AND ((SELECT COUNT(1) FROM incentives.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL) = 2
+OR (SELECT COUNT(1) FROM archive.EmploymentCheck ec WHERE ec.ApprenticeshipIncentiveId = x.Id AND ec.Result IS NOT NULL) = 2)
