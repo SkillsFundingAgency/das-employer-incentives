780639fe455436a3e168492fe181355d638315f3
David-B-Read <david.read@digital.education.gov.uk>
2022-10-07 11:07:07 +0100

    EI-1965 fix acceptance test

--- src/tests/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess.AcceptanceTests/Steps/EmploymentCheckSteps.cs
+++ src/tests/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess.AcceptanceTests/Steps/EmploymentCheckSteps.cs
@@ -400,8 +400,6 @@ namespace SFA.DAS.EmployerIncentives.Functions.PaymentsProcess.AcceptanceTests.S
             {
                 employmentCheck.Result.Should().BeNull();
             }
-
-            dbConnection.GetAll<EmploymentCheck>().Any(x => x.CheckType == EmploymentCheckType.EmployedAt365PaymentDueDateFirstCheck).Should().BeFalse();
         }
 
         private void VerifyEmployedPriorToPhaseCheck(DateTime minDate, DateTime maxDate)
