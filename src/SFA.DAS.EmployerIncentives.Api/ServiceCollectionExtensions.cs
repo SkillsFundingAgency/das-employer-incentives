50516ee2b074daa81cf5a46d7ff59b786d771c8e
Kevin Stephens <MSDN@Stephek.co.uk>
2023-03-16 14:57:48 +0000

    EI-1654-update-nlog

--- src/SFA.DAS.EmployerIncentives.Api/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.EmployerIncentives.Api/ServiceCollectionExtensions.cs
@@ -15,10 +15,10 @@ namespace SFA.DAS.EmployerIncentives.Api
         public static IServiceCollection AddNLog(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
             var env = Environment.GetEnvironmentVariable("EnvironmentName");
-            var configFileName = "NLog.config";
+            var configFileName = "nlog.config";
             if (string.IsNullOrEmpty(env) || env.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase))
             {
-                configFileName = "NLog.local.config";
+                configFileName = "nlog.local.config";
             }
             var rootDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), ".."));
             var configFilePath = Directory.GetFiles(rootDirectory, configFileName, SearchOption.AllDirectories)[0];
--- src/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.EmployerIncentives.Functions.PaymentsProcess/ServiceCollectionExtensions.cs
@@ -28,10 +28,10 @@ namespace SFA.DAS.EmployerIncentives.Functions.PaymentsProcess
         {
 
             var env = Environment.GetEnvironmentVariable("EnvironmentName");
-            var configFileName = "NLog.config";
+            var configFileName = "nlog.config";
             if (string.IsNullOrEmpty(env) || env.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase))
             {
-                configFileName = "NLog.local.config";
+                configFileName = "nlog.local.config";
             }
             var rootDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), ".."));
             var configFilePath = Directory.GetFiles(rootDirectory, configFileName, SearchOption.AllDirectories)[0];
